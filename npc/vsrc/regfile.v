a459de8c24ca00fd74fe755f6fdc3cb5464ba710
tracer-ysyx <tracer@ysyx.org>
2025-11-26 23:46:51 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:51 up 1 day,  3:18,  5 users,  load average: 0.51, 0.78, 0.80

--- npc/vsrc/regfile.v
+++ npc/vsrc/regfile.v
@@ -79,7 +79,7 @@ module regfile
         end
     end
 
-    always @(*) begin
+    always @(posedge clk or posedge rst) begin
         if(I_inst != `ZeroWord || (inst_reg != I_inst || pc_reg != I_inst_addr))
             cpu_value(1, I_inst, I_inst_addr,
                   regs[0],  regs[1],  regs[2],  regs[3],  regs[4],  regs[5],  regs[6],  regs[7],
