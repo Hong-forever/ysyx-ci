10efb0aaf41d1c559d6ac45354d397d5024bb4b6
tracer-ysyx <tracer@ysyx.org>
2025-11-26 22:30:58 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:58 up 1 day,  2:02,  5 users,  load average: 0.82, 0.48, 0.30

--- npc/csrc/utils/disasm.c
+++ npc/csrc/utils/disasm.c
@@ -13,6 +13,7 @@ static csh handle;
 
 void init_disasm() {
   void *dl_handle;
+  printf("init disasm\n");
   dl_handle = dlopen("/home/hhh/Desktop/ysyx/ysyx-workbench/npc/tools/capstone/repo/libcapstone.so.5", RTLD_LAZY);
   assert(dl_handle);
 
