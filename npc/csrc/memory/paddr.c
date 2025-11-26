88336ffeb2ed8eca3d7218fe2294135546934b19
tracer-ysyx <tracer@ysyx.org>
2025-11-27 00:59:51 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:59:51 up 1 day,  4:31,  5 users,  load average: 0.58, 0.71, 0.51

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -21,7 +21,7 @@ static uint32_t tra_mask(uint32_t wmask)
 extern "C" uint32_t pmem_read(uint32_t raddr)
 {
     if (raddr >= CONFIG_MTRACE_BASE && raddr < CONFIG_MTRACE_BASE + CONFIG_MTRACE_SIZE) {
-        IFDEF(CONFIG_MTRACE, printf(COLOR_BLUE"[Mtrace] Read addr: 0x%08x data: 0x%08x\n"COLOR_END, raddr, pmem[raddr >> 2]));
+        IFDEF(CONFIG_MTRACE, printf(COLOR_BLUE "[Mtrace] Read addr: 0x%08x data: 0x%08x\n" COLOR_END, raddr, pmem[raddr >> 2]));
     }
     raddr -= RESET_VECTOR;
     // printf("data: 0x%x addr: 0x%x\n", pmem[raddr>>2], raddr);
@@ -35,7 +35,7 @@ extern "C" uint32_t pmem_read(uint32_t raddr)
 extern "C" void pmem_write(uint32_t waddr, uint32_t wdata, uint32_t wmask)
 {
     if (waddr >= CONFIG_MTRACE_BASE && waddr < CONFIG_MTRACE_BASE + CONFIG_MTRACE_SIZE) {
-        IFDEF(CONFIG_MTRACE, printf(COLOR_BLUE"[Mtrace] Wrtie addr: 0x%08x data: 0x%08x mask: 0x%08x\n"COLOR_END, waddr, wdata, tra_mask(wmask)));
+        IFDEF(CONFIG_MTRACE, printf(COLOR_BLUE "[Mtrace] Wrtie addr: 0x%08x data: 0x%08x mask: 0x%08x\n" COLOR_END, waddr, wdata, tra_mask(wmask)));
     }
     waddr -= RESET_VECTOR;
     // printf("waddr: 0x%08x\nwdata: 0x%08x\nmask:0x%08x\n", waddr, wdata, tra_mask(wmask));
