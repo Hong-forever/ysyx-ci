09ab4ae88442d9662d3860f989ee7c6a9a1b9aa6
tracer-ysyx <tracer@ysyx.org>
2025-11-26 23:52:20 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:52:20 up 1 day,  3:23,  5 users,  load average: 1.12, 0.71, 0.74

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -185,6 +185,7 @@ static void execute(uint64_t n)
         if(cpu_inst_valid) {
             trace_and_difftest(s);
             cpu_inst_valid = 0;
+            n++;
         }
 
         if (npc_state.state == NPC_STOP || npc_state.state == NPC_ABORT) {
