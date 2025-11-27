7c4a090321c866ddae94fd451c27f448f9808a63
tracer-ysyx <tracer@ysyx.org>
2025-11-27 23:20:16 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:16 up 1 day, 18:05,  5 users,  load average: 0.79, 0.80, 0.54

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -80,8 +80,8 @@ void init_difftest(char *ref_so_file, long img_size, int port)
 
     ref_difftest_init(port);
     ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-    // ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-    // printf("cpu.pc = 0x%08x\n", cpu_r.pc);
+    ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+    printf("cpu.pc = 0x%08x\n", cpu.pc);
 }
 
 static void checkregs(CPU_state *ref, paddr_t pc)
