93c863265c8070d9b92e859799979db767467f64
tracer-ysyx <tracer@ysyx.org>
2025-11-27 22:14:21 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:21 up 1 day, 16:59,  5 users,  load average: 1.63, 1.16, 0.79

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -32,6 +32,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction)
 {
     if (direction == DIFFTEST_TO_REF) {
         cpu.pc = ((CPU_state *)dut)->pc;
+        printf("difftest_regcpy to ref: pc=0x%08x, dutpc = 0x%08x\n", cpu.pc, ((CPU_state *)dut)->pc);
         for (int i = 0; i < 32; i++) {
             cpu.gpr[i] = ((CPU_state *)dut)->gpr[i];
         }
--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -82,7 +82,7 @@ void init_difftest(char *ref_so_file, long img_size, int port)
 
     ref_difftest_init(port);
     ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-    // ref_difftest_regcpy(&cpu_r, DIFFTEST_TO_REF);
+    ref_difftest_regcpy(&cpu_r, DIFFTEST_TO_REF);
 }
 
 static void checkregs(CPU_state *ref, paddr_t pc)
