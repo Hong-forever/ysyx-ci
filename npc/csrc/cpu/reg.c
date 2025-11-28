4cd2ae3909932144081356ca349622836a7a3640
tracer-ysyx <tracer@ysyx.org>
2025-11-28 15:11:00 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:00 up 1 day, 20:00,  5 users,  load average: 1.18, 0.65, 0.52

--- npc/csrc/cpu/reg.c
+++ npc/csrc/cpu/reg.c
@@ -69,12 +69,12 @@ bool difftest_checkregs(CPU_state *ref_r, paddr_t pc) {
 
     if(ref_r->pc != cpu.pc) {
         flag = false;
-        printf(COLOR_RED "DIFF==>> ref cpu.pc: 0x%08x, dut cpu.pc: 0x%08x\n" COLOR_END, ref_r->pc, cpu.pc);
+        printf(COLOR_RED "DIFF==>> ref pc: 0x%08x, dut pc: 0x%08x\n" COLOR_END, ref_r->pc, cpu.pc);
     }
 
     for(int i=0; i<32; i++) {
         if(ref_r->gpr[i] != cpu.gpr[i]) {
-            printf(COLOR_RED "DIFF==>> ref cpu.gpr[%d]: 0x%08x, dut cpu.gpr[%d]: 0x%08x\n" COLOR_END, i, ref_r->gpr[i], i, cpu.gpr[i]);
+            printf(COLOR_RED "DIFF==>> ref gpr[%d]: 0x%08x, dut gpr[%d]: 0x%08x\n" COLOR_END, i, ref_r->gpr[i], i, cpu.gpr[i]);
             flag = false;
             break;
         } 
