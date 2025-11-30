88fbf67b536ae4c082970fde362e0704919801ad
tracer-ysyx <tracer@ysyx.org>
2025-11-30 15:08:23 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:23 up 2 days, 12:35,  1 user,  load average: 1.40, 1.29, 1.05

--- npc/vsrc/riscv_ic_sopc.v
+++ npc/vsrc/riscv_ic_sopc.v
@@ -62,6 +62,11 @@ module top
     assign ibus_rdata = ibus_req ? paddr_read(ibus_addr) : `ZeroWord;
     assign dbus_rdata = dbus_req ? paddr_read(dbus_addr) : `ZeroWord;
 
+    wire [31:0] pc = ibus_addr;
+    initial begin
+        $monitor("npc: pc=0x%08x", pc);
+    end
+
     always @(*) begin
         if (dbus_req && dbus_we) begin
             paddr_write(dbus_addr, dbus_wdata, {28'b0, dbus_mask});
