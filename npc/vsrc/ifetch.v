47be72e94e2369bd2584465c62db527ed42d5f50
tracer-ysyx <tracer@ysyx.org>
2025-11-30 15:19:18 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:18 up 2 days, 12:46,  1 user,  load average: 0.88, 0.82, 0.92

--- npc/vsrc/ifetch.v
+++ npc/vsrc/ifetch.v
@@ -38,14 +38,14 @@ module ifetch
     wire [`InstAddrBus] npc;
     wire [`InstAddrBus] pc_plus4;
 
-    reg ibus_req;
-    always @(posedge clk or posedge rst) begin
-        if(rst) begin
-            ibus_req <= `False;
-        end else begin
-            ibus_req <= ~I_jtag_halt;
-        end
-    end
+    // reg ibus_req;
+    // always @(posedge clk or posedge rst) begin
+    //     if(rst) begin
+    //         ibus_req <= `False;
+    //     end else begin
+    //         ibus_req <= ~I_jtag_halt;
+    //     end
+    // end
 
     // 取指PC
     reg [`InstAddrBus] pc;
@@ -57,8 +57,7 @@ module ifetch
         end
     end
 
-    assign npc = rst          ? `RomAddrBase    :
-                 I_flush      ? I_flush_addr    :
+    assign npc = I_flush      ? I_flush_addr    :
                  I_stall      ? pc              :
                  I_bru_taken  ? I_bru_target    :
                  pc_plus4;
@@ -68,7 +67,7 @@ module ifetch
     assign O_inst = I_ibus_data;
     assign O_inst_addr = pc;
 
-    assign O_ibus_req = ibus_req;
+    assign O_ibus_req = ~rst;
     assign O_ibus_we = `False;
     assign O_ibus_addr = pc;
     assign O_ibus_data = `ZeroWord;
