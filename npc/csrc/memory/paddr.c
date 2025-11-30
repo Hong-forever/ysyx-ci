6f76b49481bd9c2fc0319f436bc8bc2e623e7bb0
tracer-ysyx <tracer@ysyx.org>
2025-11-30 15:44:24 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:24 up 2 days, 13:11,  1 user,  load average: 0.23, 0.67, 0.74

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -25,9 +25,9 @@ paddr_t *guest_to_host(paddr_t paddr) {
     return pmem + ((paddr - CONFIG_MBASE) >> 2);
 }
 
-// paddr_t host_to_guest(paddr_t *haddr) {
-//     return haddr - pmem + CONFIG_MBASE;
-// }
+paddr_t host_to_guest(paddr_t *haddr) {
+    return ((haddr - pmem) << 2) + CONFIG_MBASE;
+}
 
 static void out_of_bound(paddr_t addr, bool is_write) {
     printf(COLOR_RED "%s address = 0x%08x is out of bound of pmem [0x%08x, 0x%08x]\n" COLOR_END, is_write?"Write":"Read", addr, PMEM_LEFT, PMEM_RIGHT);
