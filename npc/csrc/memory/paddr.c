f555807442888fb7f62ec840e48aa16e21d236d8
tracer-ysyx <tracer@ysyx.org>
2025-11-30 22:14:07 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:07 up 2 days, 19:41,  1 user,  load average: 1.21, 1.06, 0.85

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -90,6 +90,7 @@ extern "C" word_t paddr_read(paddr_t raddr) {
     else if ((raddr&~0x7u) == RTC_MMIO) {
         uint64_t ns = get_time();
     }
+    printf("0x%08x\n", raddr&~0x7u);
 
     out_of_bound(raddr, false);
     return 0;
