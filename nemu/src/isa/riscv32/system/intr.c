fa49cfdb81ed28007c8cbaf554953bfe56e998b9
tracer-ysyx <tracer@ysyx.org>
2025-12-01 23:34:53 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:34:53 up 3 days,  1:51,  1 user,  load average: 1.39, 0.89, 0.84

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,9 +19,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  mstatus = NO;
+  mcause = NO;
+  mepc = epc;
 
-  return 0;
+  return mtvec;
 }
 
 word_t isa_query_intr() {
