d48484582ef3112bb662fd8082a068b707820167
tracer-ysyx <tracer@ysyx.org>
2025-12-02 17:20:16 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:16 up 3 days,  7:30,  1 user,  load average: 0.63, 0.85, 0.63

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -24,8 +24,6 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
-  printf("%p\n", __am_asm_trap);
-
   // register event handler
   user_handler = handler;
 
