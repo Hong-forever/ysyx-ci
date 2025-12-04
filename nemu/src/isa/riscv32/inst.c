2fc2de6e32bbbbd0845ec8ed7645ede9dce772cb
tracer-ysyx <tracer@ysyx.org>
2025-12-04 17:58:43 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:43 up 18:21,  1 user,  load average: 0.21, 0.41, 0.45

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -47,7 +47,7 @@ enum {
     dnpc = isa_raise_intr(11, s->pc); \
   } while(0)
 
-#define MRET(dnpc) do { dnpc = Mepc(); Mstatus() = (((Mstatus() >> 4) & 0x8) | (Mstatus() | 0x80)) & ~0x88; } while(0)
+#define MRET(dnpc) do { dnpc = Mepc(); Mstatus() = (((Mstatus() >> 4) & 0x8) | (Mstatus() | 0x80)) & 0x88; } while(0)
 
 int rs1 = 0;
 int rs2 = 0;
