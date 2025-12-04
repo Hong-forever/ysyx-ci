ccbdbadc00e79c20e629107572e26d6e6a24f5fd
tracer-ysyx <tracer@ysyx.org>
2025-12-04 17:51:19 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:51:19 up 18:14,  1 user,  load average: 0.79, 0.62, 0.50

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -39,7 +39,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     CHECKDIFF(csr.mtvec,   "mtvec  ");
 
     if(!flag) {
-        printf("Difftest: Error at pc: 0x%08x\n", cpu.pc);
+        printf("Difftest: Error at pc: 0x%08x\n", pc);
         return false;
     }
 
