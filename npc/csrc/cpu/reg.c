3d2e89e6f117a9582b5ed685460e712f4c0029b7
ysyx-hhh <3224318821@qq.com>
2025-12-15 01:52:19 +0800

    delete pipeline

--- npc/csrc/cpu/reg.c
+++ npc/csrc/cpu/reg.c
@@ -2,11 +2,10 @@
 #include "utils.h"
 
 const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
+    "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 CPU_state cpu = {};
 Extra_CPU_state extra_cpu = {};
@@ -24,29 +23,53 @@ extern "C" void cpu_value(int diff_skip_flag, int valid, int inst, int inst_addr
                           int gpr20, int gpr21, int gpr22, int gpr23,
                           int gpr24, int gpr25, int gpr26, int gpr27,
                           int gpr28, int gpr29, int gpr30, int gpr31,
-                          int mepc, int mtvec, int mstatus, 
-                          int mcause, int mcyclel, int mcycleh, 
-                          int mvendorid, int marchid
-)
+                          int mepc, int mtvec, int mstatus,
+                          int mcause, int mcyclel, int mcycleh,
+                          int mvendorid, int marchid)
 {
     // printf("dut npc: pc=0x%08x\n", pc);
     cpu_inst_valid = valid;
-    s.inst = inst; s.pc = inst_addr;
+    s.inst = inst;
+    s.pc = inst_addr;
 
-    if(diff_skip_flag) {
+    if (diff_skip_flag) {
         // printf("Difftest: skip ref at pc: 0x%08x\n", s.pc);
         IFDEF(CONFIG_DIFFTEST, difftest_skip_ref());
     }
 
     cpu.pc = pc;
-    cpu.gpr[0] = gpr0; cpu.gpr[1] = gpr1; cpu.gpr[2] = gpr2; cpu.gpr[3] = gpr3;
-    cpu.gpr[4] = gpr4; cpu.gpr[5] = gpr5; cpu.gpr[6] = gpr6; cpu.gpr[7] = gpr7;
-    cpu.gpr[8] = gpr8; cpu.gpr[9] = gpr9; cpu.gpr[10] = gpr10; cpu.gpr[11] = gpr11;
-    cpu.gpr[12] = gpr12; cpu.gpr[13] = gpr13; cpu.gpr[14] = gpr14; cpu.gpr[15] = gpr15;
-    cpu.gpr[16] = gpr16; cpu.gpr[17] = gpr17; cpu.gpr[18] = gpr18; cpu.gpr[19] = gpr19;
-    cpu.gpr[20] = gpr20; cpu.gpr[21] = gpr21; cpu.gpr[22] = gpr22; cpu.gpr[23] = gpr23;
-    cpu.gpr[24] = gpr24; cpu.gpr[25] = gpr25; cpu.gpr[26] = gpr26; cpu.gpr[27] = gpr27;
-    cpu.gpr[28] = gpr28; cpu.gpr[29] = gpr29; cpu.gpr[30] = gpr30; cpu.gpr[31] = gpr31;
+    cpu.gpr[0] = gpr0;
+    cpu.gpr[1] = gpr1;
+    cpu.gpr[2] = gpr2;
+    cpu.gpr[3] = gpr3;
+    cpu.gpr[4] = gpr4;
+    cpu.gpr[5] = gpr5;
+    cpu.gpr[6] = gpr6;
+    cpu.gpr[7] = gpr7;
+    cpu.gpr[8] = gpr8;
+    cpu.gpr[9] = gpr9;
+    cpu.gpr[10] = gpr10;
+    cpu.gpr[11] = gpr11;
+    cpu.gpr[12] = gpr12;
+    cpu.gpr[13] = gpr13;
+    cpu.gpr[14] = gpr14;
+    cpu.gpr[15] = gpr15;
+    cpu.gpr[16] = gpr16;
+    cpu.gpr[17] = gpr17;
+    cpu.gpr[18] = gpr18;
+    cpu.gpr[19] = gpr19;
+    cpu.gpr[20] = gpr20;
+    cpu.gpr[21] = gpr21;
+    cpu.gpr[22] = gpr22;
+    cpu.gpr[23] = gpr23;
+    cpu.gpr[24] = gpr24;
+    cpu.gpr[25] = gpr25;
+    cpu.gpr[26] = gpr26;
+    cpu.gpr[27] = gpr27;
+    cpu.gpr[28] = gpr28;
+    cpu.gpr[29] = gpr29;
+    cpu.gpr[30] = gpr30;
+    cpu.gpr[31] = gpr31;
 
     cpu.csr.mepc = mepc;
     cpu.csr.mtvec = mtvec;
@@ -119,18 +142,19 @@ word_t reg_str2val(const char *s, bool *success)
     return 0;
 }
 
-#define CHECKDIFF(reg, fmt, ...) \
-  if (ref_r->reg != cpu.reg) { \
-    PRINTF_RED("[DIFF]==>> " fmt " got: 0x%08x, but expected: 0x%08x\n", ##__VA_ARGS__, cpu.reg, ref_r->reg); \
-    flag = false; \
-  }
+#define CHECKDIFF(reg,fmt, ...) \
+    if (ref_r->reg != cpu.reg){ \
+        PRINTF_RED("[DIFF]==>> " fmt " got: 0x%08x, but expected: 0x%08x\n", ##__VA_ARGS__, cpu.reg, ref_r->reg); \
+        flag = false;        \
+    }
 
-bool difftest_checkregs(CPU_state *ref_r, paddr_t pc) {
+bool difftest_checkregs(CPU_state *ref_r, paddr_t pc)
+{
     bool flag = true;
 
     CHECKDIFF(pc, "pc");
 
-    for(int i=0; i<32; i++) {
+    for (int i = 0; i < 32; i++) {
         CHECKDIFF(gpr[i], "gpr[%02d]", i);
     }
 
@@ -139,7 +163,7 @@ bool difftest_checkregs(CPU_state *ref_r, paddr_t pc) {
     CHECKDIFF(csr.mepc, "mepc");
     CHECKDIFF(csr.mtvec, "mtvec");
 
-    if(!flag) {
+    if (!flag) {
         PRINTF_RED("Difftest: Error at pc: 0x%08x\n", pc);
         return false;
     }
