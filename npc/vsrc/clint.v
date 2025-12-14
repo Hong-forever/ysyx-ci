c382f9d1a37e6c32beeed6d12356b5bc2d0192f3
tracer-ysyx <tracer@ysyx.org>
2025-12-15 01:35:13 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:35:13 up 4 days,  3:52,  1 user,  load average: 0.76, 0.69, 0.81

--- npc/vsrc/clint.v
+++ npc/vsrc/clint.v
@@ -191,8 +191,7 @@ module clint
                         `ZeroWord;
 
     initial begin
-        $monitor("is_ecall: %b, is_ebreak: %b, is_mret: %b, int_state: %b, csr_state: %b, csr_we: %b, csr_waddr: %h, csr_wdata: %h, O_stallreq: %b, O_flush: %b, O_flush_addr: %h\n",
-                 is_ecall, is_ebreak, is_mret, int_state, csr_state, csr_we, csr_waddr, csr_wdata, O_stallreq, O_flush, O_flush_addr);
+        $monitor("is_ecall: %b\n", is_ecall);
     end
 
 endmodule
