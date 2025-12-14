a5fbce96df32fa6200762bf7ced62a84c90e891f
tracer-ysyx <tracer@ysyx.org>
2025-12-14 23:45:41 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:45:41 up 4 days,  2:02,  1 user,  load average: 0.82, 1.01, 0.88

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -21,6 +21,7 @@ void halt(int code)
 void _trm_init()
 {
     uint32_t ysyx_logo = npc_csr_read(CSR_MVENDORID);
+    printf("mvendorid: 0x%08x\n", ysyx_logo);
     uint32_t stu_num = npc_csr_read(CSR_MARCHID);
     printf("Welcome to %s!\n", ysyx_logo == YSYX_LOGO ? "NJU-YSYX" : "Unknown");
     printf("Student Number: %u\n", stu_num);
