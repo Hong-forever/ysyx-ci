f180f5e56cd6bf46a9a706e4b15d0701ab28fe9d
tracer-ysyx <tracer@ysyx.org>
2025-12-15 01:32:19 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:32:19 up 4 days,  3:49,  1 user,  load average: 0.59, 0.69, 0.84

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -41,6 +41,7 @@ bool cte_init(Context *(*handler)(Event, Context *))
 {
     // initialize exception entry
     asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+    printf("mtvec set to %p\n", __am_asm_trap);
 
     // register event handler
     user_handler = handler;
