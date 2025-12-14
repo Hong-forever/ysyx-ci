488990d74e5235ad7dca12b66691a16209ff9791
tracer-ysyx <tracer@ysyx.org>
2025-12-15 00:04:21 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:04:21 up 4 days,  2:21,  1 user,  load average: 1.61, 1.61, 1.28

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -166,7 +166,7 @@ static void exec_once()
     single_cycle();
 
     // printf("cpu_inst_valid: %d\n", cpu_inst_valid);
-    printf("mcycle: %lu\n", extra_cpu.mcyclel + ((uint64_t)extra_cpu.mcycleh << 32));
+    // printf("mcycle: %lu\n", extra_cpu.mcyclel + ((uint64_t)extra_cpu.mcycleh << 32));
     
     if (!cpu_inst_valid) return;
 
