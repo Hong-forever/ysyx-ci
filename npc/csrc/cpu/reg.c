407bd762ae2ef2769a38405ef97637d71d2a3c68
tracer-ysyx <tracer@ysyx.org>
2025-12-14 23:44:46 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:44:46 up 4 days,  2:01,  1 user,  load average: 1.45, 1.12, 0.90

--- npc/csrc/cpu/reg.c
+++ npc/csrc/cpu/reg.c
@@ -55,7 +55,7 @@ extern "C" void cpu_value(int diff_skip_flag, int valid, int inst, int inst_addr
     extra_cpu.mcycleh = mcycleh;
     extra_cpu.mvendorid = mvendorid;
     extra_cpu.marchid = marchid;
-    // printf("pc=0x%08x inst=0x%08x\n", s.pc, s.inst);
+    printf("mvendorid: 0x%08x, marchid: 0x%08x\n", extra_cpu.mvendorid, extra_cpu.marchid);
 }
 
 void reg_display()
