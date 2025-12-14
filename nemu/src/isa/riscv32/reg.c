ae50ff83ef8f6f4155c2fc145be84bf0c9187425
tracer-ysyx <tracer@ysyx.org>
2025-12-15 00:20:40 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:40 up 4 days,  2:37,  1 user,  load average: 0.95, 0.90, 1.04

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,7 +26,7 @@ const char *regs[] = {
 void isa_reg_display() {
     printf("[==> PC ==]  : 0x%08x\n", cpu.pc);
     for(int i=0; i<MUXDEF(CONFIG_RVE, 16, 32); i++) {
-        printf("regs[%02d]-%-4s: 0x%08x\n", i, reg_name(i), gpr(i));
+        printf("gpr[%02d]-%-4s: 0x%08x\n", i, reg_name(i), gpr(i));
     }
     printf("mstatus  : 0x%08x\n", Mstatus());
     printf("csr-mcause   : 0x%08x\n", Mcause());
