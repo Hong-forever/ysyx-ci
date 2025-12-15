3bf5d661ead76998db5abd23053c57508d85f99a
tracer-ysyx <tracer@ysyx.org>
2025-12-15 22:02:59 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:59 up 4 days, 18:11,  1 user,  load average: 1.15, 1.45, 1.26

--- npc/csrc/include/utils.h
+++ npc/csrc/include/utils.h
@@ -25,6 +25,7 @@ extern NPCState npc_state;
 #define PRINTF_BLUE(fmt, ...) \
     do { \
         printf(COLOR_BLUE fmt COLOR_END, ##__VA_ARGS__); \
+        printf("\n"); \
     } while (0)
 
 #define PRINTF_GREEN(fmt, ...) \
