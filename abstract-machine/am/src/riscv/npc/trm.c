5964a2bc131955229fc3d033b586557c5910f62d
tracer-ysyx <tracer@ysyx.org>
2025-12-16 23:12:10 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:12:10 up 5 days,  6:41,  1 user,  load average: 1.03, 0.80, 0.53

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -26,6 +26,7 @@ void _trm_init()
     printf("========================================\n");
     printf("Welcome to %s!\n", ysyx_logo == YSYX_LOGO ? "YSYX" : "Unknown");
     printf("Student Number: ysyx_%x\n", stu_num);
+    printf("Name: %s\n", "HUANG HAIHONG");
     printf("========================================\n");
     
     int ret = main(mainargs);
