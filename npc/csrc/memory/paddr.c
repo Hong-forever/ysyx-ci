d6b1d51ab0e2f5b2931cd500b5d4c0c73e229279
tracer-ysyx <tracer@ysyx.org>
2025-12-16 22:31:39 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:39 up 5 days,  6:00,  1 user,  load average: 0.30, 0.60, 0.73

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -106,7 +106,6 @@ extern "C" word_t paddr_read(paddr_t raddr) {
         }
 
         out_of_bound(raddr, false);
-        npc_state.state = NPC_ABORT;
         return 0;
     }
 }
@@ -122,7 +121,6 @@ extern "C" void paddr_write(paddr_t waddr, word_t wdata, uint32_t wmask) {
             putc((char)(wdata & 0xff), stderr);
         } else {
             out_of_bound(waddr, true);
-            npc_state.state = NPC_ABORT;
         }
     }
 }
