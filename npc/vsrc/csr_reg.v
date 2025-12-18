5d625a289d85c6044228d1e07e26805ae75cf642
tracer-ysyx <tracer@ysyx.org>
2025-12-18 18:12:43 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:12:43 up  4:01,  1 user,  load average: 1.04, 0.77, 0.63

--- npc/vsrc/csr_reg.v
+++ npc/vsrc/csr_reg.v
@@ -80,6 +80,19 @@ module csr_reg
     wire except_call = except_sync | except_async;
     wire except_mret = is_mret;
 
+    reg e_sync_r, e_async_r, e_mret_r;
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            e_sync_r <= 1'b0;
+            e_async_r <= 1'b0;
+            e_mret_r <= 1'b0;
+        end else begin
+            e_sync_r <= except_sync;
+            e_async_r <= except_async;
+            e_mret_r <= except_mret;
+        end
+    end
+    
 
     `define YSYX_LOGO      32'h79737978 //ysyx的logo
     `define YSYX_STU_NUM   32'h25110270 //我的学号-25110270
@@ -119,15 +132,15 @@ module csr_reg
             mvendorid <= `YSYX_LOGO;
             marchid <= `YSYX_STU_NUM;
         end else begin
-            if(except_async & ~I_valid) begin
+            if(except_async & ~e_async_r) begin
                 mepc <= ext_int_addr;
                 mcause <= 32'h80000004; //定时器中断
                 mstatus <= {mstatus[31:8], mstatus[3], mstatus[6:4], 1'b0, mstatus[2:0]} | 32'h1800; //MPIE->MIE, MIE清0
-            end else if(except_sync & ~I_valid) begin
+            end else if(except_sync & ~e_sync_r) begin
                 mepc <= I_except_addr;
                 mcause <= is_ecall ? 32'd11 : 32'd3; //ecall=11, ebreak=3
                 mstatus <= {mstatus[31:8], mstatus[3], mstatus[6:4], 1'b0, mstatus[2:0]} | 32'h1800; //MPIE->MIE, MIE清0
-            end else if(except_mret & ~I_valid) begin
+            end else if(except_mret & ~e_mret_r) begin
                 mstatus <= {mstatus[31:8], 1'b1, mstatus[6:4], mstatus[7], mstatus[2:0]} & ~32'h1800; //MIE<-MPIE
             end else begin    
                 if(I_we) begin
