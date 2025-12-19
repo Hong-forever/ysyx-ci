42f3eff8b6a1147e65634ad3abe27b4b1b48f8bb
tracer-ysyx <tracer@ysyx.org>
2025-12-19 19:30:04 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:04 up 15:29,  1 user,  load average: 0.94, 0.47, 0.19

--- npc/vsrc/lfsr.v
+++ npc/vsrc/lfsr.v
@@ -17,14 +17,14 @@ module lfsr
     reg [WIDTH-1:0] lfsr_reg;
     wire feedback;
 
-    assign feedback = lfsr_reg[4] ^ lfsr_reg[2] ^ lfsr_reg[1] ^ lfsr_reg[0];
+    assign feedback = lfsr_reg[4] ^ lfsr_reg[3] ^ lfsr_reg[2] ^ lfsr_reg[0];
     assign O_random = lfsr_reg;
 
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             lfsr_reg <= I_seed;
         end else begin
-            lfsr_reg <= {feedback, lfsr_reg[WIDTH-2:0]};
+            lfsr_reg <= {feedback, lfsr_reg[WIDTH-1:1]};
         end
     end
 endmodule
