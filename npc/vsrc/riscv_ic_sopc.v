538afb50b1c6b5d201db953b569996d0d8a2db69
ysyx-hhh <3224318821@qq.com>
2025-12-20 01:43:14 +0800

    11

--- npc/vsrc/riscv_ic_sopc.v
+++ npc/vsrc/riscv_ic_sopc.v
@@ -239,7 +239,7 @@ module top
     (
         .clk                    (clk                        ),
         .rst_n                  (rst_n                      ),
-        .I_seed                 (8'h1                       ),
+        .I_seed                 (8'h0                       ),
         .O_random               (irandom                    )
     );
 
@@ -249,7 +249,7 @@ module top
     (
         .clk                    (clk                        ),
         .rst_n                  (rst_n                      ),
-        .I_seed                 (8'h2                       ),
+        .I_seed                 (8'h0                       ),
         .O_random               (drandom                    )
     );
     
@@ -259,7 +259,7 @@ module top
     (
         .clk                    (clk                        ),
         .rst_n                  (rst_n                      ),
-        .I_seed                 (8'h3                       ),
+        .I_seed                 (8'h0                       ),
         .O_random               (irandom2                   )
     );
 
@@ -269,7 +269,7 @@ module top
     (
         .clk                    (clk                        ),
         .rst_n                  (rst_n                      ),
-        .I_seed                 (8'h4                       ),
+        .I_seed                 (8'h0                       ),
         .O_random               (drandom2                   )
     );
 
