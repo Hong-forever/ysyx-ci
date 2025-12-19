63801419c7c62051842dfb15546fd2f4970f19ef
ysyx-hhh <3224318821@qq.com>
2025-12-19 20:31:48 +0800

    simple bus lfsr delay

--- npc/vsrc/riscv_ic_sopc.v
+++ npc/vsrc/riscv_ic_sopc.v
@@ -60,31 +60,100 @@ module top
     import "DPI-C" function int paddr_read(input int raddr);
     import "DPI-C" function void paddr_write(input int waddr, input int wdata, input int wmask);
 
-    `define ISTALL_PERIOD 5
-    `define DSTALL_PERIOD 2
+    `define RAMDOM_WIDTH 8
+    wire [`RAMDOM_WIDTH-1:0] irandom, irandom2, drandom, drandom2;
+    lfsr #(
+        .WIDTH                  (`RAMDOM_WIDTH              )      
+    ) ilfsr_inst
+    (
+        .clk                    (clk                        ),
+        .rst_n                  (rst_n                      ),
+        .I_seed                 (8'h2                       ),
+        .O_random               (irandom                    )
+    );
 
-    reg [`ISTALL_PERIOD-1:0] ibus_req_r;
-    reg [`DSTALL_PERIOD-1:0] dbus_req_r;
+    lfsr #(
+        .WIDTH                  (`RAMDOM_WIDTH              )      
+    ) dlfsr_inst
+    (
+        .clk                    (clk                        ),
+        .rst_n                  (rst_n                      ),
+        .I_seed                 (8'h3                       ),
+        .O_random               (drandom                    )
+    );
+    
+    lfsr #(
+        .WIDTH                  (`RAMDOM_WIDTH              )      
+    ) ilfsr_inst2
+    (
+        .clk                    (clk                        ),
+        .rst_n                  (rst_n                      ),
+        .I_seed                 (8'h4                       ),
+        .O_random               (irandom2                   )
+    );
+
+    lfsr #(
+        .WIDTH                  (`RAMDOM_WIDTH              )      
+    ) dlfsr_inst2
+    (
+        .clk                    (clk                        ),
+        .rst_n                  (rst_n                      ),
+        .I_seed                 (8'h5                       ),
+        .O_random               (drandom2                   )
+    );
+
+    reg ireq, dreq, ireq_flag, dreq_flag;
+    reg [`RAMDOM_WIDTH-1:0] irandom_req, drandom_req;
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
-            ibus_req_r <= 0;
-            dbus_req_r <= 0;
+            ireq <= 1'b0;
+            ireq_flag <= 1'b0;
+            dreq <= 1'b0;
+            dreq_flag <= 1'b0;
+            irandom_req <= 0;
+            drandom_req <= 0;
         end else begin
-            ibus_req_r <= {ibus_req_r[`ISTALL_PERIOD-2:0], ibus_req};
-            dbus_req_r <= {dbus_req_r[`DSTALL_PERIOD-2:0], dbus_req};
+            if(ibus_req) begin
+                ireq <= 1'b0;
+                irandom_req <= irandom;
+                ireq_flag <= 1'b1;
+            end else begin
+                if(ireq_flag) begin
+                    irandom_req <= irandom_req - 1;
+                    if(irandom_req == 0) begin
+                        ireq <= 1'b1;
+                        ireq_flag <= 1'b0;
+                    end
+                end else begin
+                    ireq <= 1'b0;
+                end
+            end
 
-            // ibus_req_r <= ibus_req;
-            // dbus_req_r <= dbus_req;
+            if(dbus_req) begin
+                dreq <= 1'b0;
+                drandom_req <= drandom;
+                dreq_flag <= 1'b1;
+            end else begin
+                if(dreq_flag) begin
+                    drandom_req <= drandom_req - 1;
+                    if(drandom_req == 0) begin
+                        dreq <= 1'b1;
+                        dreq_flag <= 1'b0;
+                    end
+                end else begin
+                    dreq <= 1'b0;
+                end
+            end
         end
     end
 
-    reg [`InstBus] inst;
+    reg [`InstBus] inst, inst_r;
     reg inst_ready;
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             inst <= `ZeroWord;
             inst_ready <= 1'b0;
-        end else if(ibus_req_r[`ISTALL_PERIOD-1]) begin
+        end else if(ireq) begin
             inst <= paddr_read(ibus_addr);
             inst_ready <= 1'b1;
         end else begin
@@ -92,19 +161,17 @@ module top
             inst_ready <= 1'b0;
         end
     end
-    assign ibus_rdata = inst;
-    assign ibus_ready = inst_ready;
 
-    reg [`MemDataBus] data;
+    reg [`MemDataBus] data, data_r;
     reg data_ready;
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             data <= `ZeroWord;
             data_ready <= 1'b0;
-        end else if(dbus_req_r[`DSTALL_PERIOD-1] & ~dbus_we) begin
+        end else if(dreq & ~dbus_we) begin
             data <= paddr_read(dbus_addr);
             data_ready <= 1'b1;
-        end else if(dbus_req_r[`DSTALL_PERIOD-1] & dbus_we) begin
+        end else if(dreq & dbus_we) begin
             paddr_write(dbus_addr, dbus_wdata, {28'b0, dbus_mask});
             data_ready <= 1'b1;
         end else begin
@@ -113,8 +180,72 @@ module top
         end
     end
 
-    assign dbus_rdata = data;
-    assign dbus_ready = data_ready;
+    reg irdy, drdy, irdy_flag, drdy_flag;
+    reg [`RAMDOM_WIDTH-1:0] irandom_rdy, drandom_rdy;
+    reg [`InstBus] inst_rdy;
+    reg [`MemDataBus] data_rdy;
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            irdy <= 1'b0;
+            irdy_flag <= 1'b0;
+            drdy <= 1'b0;
+            drdy_flag <= 1'b0;
+            irandom_rdy <= 0;
+            drandom_rdy <= 0;
+            inst_r <= `ZeroWord;
+            data_r <= `ZeroWord;
+            inst_rdy <= `ZeroWord;
+            data_rdy <= `ZeroWord;
+        end else begin
+            if(inst_ready) begin
+                irdy <= 1'b0;
+                irandom_rdy <= irandom2;
+                irdy_flag <= 1'b1;
+                inst_r <= inst;
+                inst_rdy <= `ZeroWord;
+            end else begin
+                if(irdy_flag) begin
+                    irandom_rdy <= irandom_rdy - 1;
+                    if(irandom_rdy == 0) begin
+                        irdy <= 1'b1;
+                        irdy_flag <= 1'b0;
+                        inst_r <= `ZeroWord;
+                        inst_rdy <= inst_r;
+                    end
+                end else begin
+                    irdy <= 1'b0;
+                    inst_rdy <= `ZeroWord;
+                end
+            end
+
+            if(data_ready) begin
+                drdy <= 1'b0;
+                drandom_rdy <= drandom2;
+                drdy_flag <= 1'b1;
+                data_r <= data;
+                data_rdy <= `ZeroWord;
+            end else begin
+                if(drdy_flag) begin
+                    drandom_rdy <= drandom_rdy - 1;
+                    if(drandom_rdy == 0) begin
+                        drdy <= 1'b1;
+                        drdy_flag <= 1'b0;
+                        data_r <= `ZeroWord;
+                        data_rdy <= data_r;
+                    end
+                end else begin
+                    drdy <= 1'b0;
+                    data_rdy <= `ZeroWord;
+                end
+            end
+        end
+    end
+
+    assign ibus_rdata = inst_rdy;
+    assign ibus_ready = irdy;
+
+    assign dbus_rdata = data_rdy;
+    assign dbus_ready = drdy;
 
     `define SERIAL_MMIO 32'h1000_0000
     `define RTC_MMIO    32'h2000_0000
