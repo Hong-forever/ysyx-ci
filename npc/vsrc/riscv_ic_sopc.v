330d403b0dfb98d1b9c313219ac5bcb9390e77a9
tracer-ysyx <tracer@ysyx.org>
2025-12-19 09:43:23 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:43:23 up 11:53,  1 user,  load average: 0.84, 0.51, 0.48

--- npc/vsrc/riscv_ic_sopc.v
+++ npc/vsrc/riscv_ic_sopc.v
@@ -60,8 +60,8 @@ module top
     import "DPI-C" function int paddr_read(input int raddr);
     import "DPI-C" function void paddr_write(input int waddr, input int wdata, input int wmask);
 
-    `define ISTALL_PERIOD 4
-    `define DSTALL_PERIOD 9
+    `define ISTALL_PERIOD 5
+    `define DSTALL_PERIOD 2
 
     reg [`ISTALL_PERIOD-1:0] ibus_req_r;
     reg [`DSTALL_PERIOD-1:0] dbus_req_r;
