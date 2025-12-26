f51657756821c18552ca63209c648fbd09cd06dc
tracer-ysyx <tracer@ysyx.org>
2025-12-26 18:31:27 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:31:27 up 2 days, 13:39,  1 user,  load average: 0.99, 0.90, 0.73

--- npc/vsrc/defines.v
+++ npc/vsrc/defines.v
@@ -6,10 +6,10 @@
 `define DPIC
 `define RAMDOM_WIDTH 8
 
-`define SEED1 8'd1
-`define SEED2 8'd2
-`define SEED3 8'd3
-`define SEED4 8'd5
+`define SEED1 8'd0
+`define SEED2 8'd0
+`define SEED3 8'd0
+`define SEED4 8'd0
 
 //------------------------------------------------------------------------
 // CLOCK
