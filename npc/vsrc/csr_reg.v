a754fabcc59c35095ef1b35a47ab45e84d4b2886
ysyx-hhh <3224318821@qq.com>
2025-12-29 17:46:50 +0800

    macro improve

--- npc/vsrc/csr_reg.v
+++ npc/vsrc/csr_reg.v
@@ -75,33 +75,22 @@ module csr_reg
     //复位撤销后就一直计数
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
-            cycle <= {`ZeroWord, `ZeroWord};
+            cycle <= `Zero;
         end else begin
             cycle <= cycle + 1'b1;
         end
     end
 
-    // reg csr_timer_int;
-    // always @(posedge clk or posedge rst) begin
-    //     if(rst) begin
-    //         csr_timer_int <= `INT_DEASSERT;
-    //     end else begin
-    //         if(mtimecmp != {`ZeroWord, `ZeroWord} && cycle == mtimecmp) begin
-    //             csr_timer_int <= `INT_ASSERT;
-    //         end
-    //     end
-    // end
-
     //write reg
     //写寄存器操作
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
-            mtvec <= `ZeroWord;
-            mcause <= `ZeroWord;
-            mepc <= `ZeroWord;
-            mie <= `ZeroWord;
-            mstatus <= `ZeroWord;
-            mscratch <= `ZeroWord;
+            mtvec <= `Zero;
+            mcause <= `Zero;
+            mepc <= `Zero;
+            mie <= `Zero;
+            mstatus <= `Zero;
+            mscratch <= `Zero;
             mvendorid <= `YSYX_LOGO;
             marchid <= `YSYX_STU_NUM;
         end else begin
@@ -146,7 +135,7 @@ module csr_reg
                 `CSR_Addr_CYCLEH:   rdata1 = cycle[63:32];
                 `CSR_Addr_MVENDORID:rdata1 = mvendorid;
                 `CSR_Addr_MARCHID:  rdata1 = marchid;
-                default:            rdata1 = `ZeroWord;
+                default:            rdata1 = `Zero;
             endcase
         end
     end
@@ -159,7 +148,7 @@ module csr_reg
 
     assign O_flush = except_call | except_mret;
     assign O_flush_addr =   except_call ? mtvec :
-                            except_mret ? mepc  : `ZeroWord;
+                            except_mret ? mepc  : `Zero;
 
     assign O_csr_mtvec = mtvec;
     assign O_csr_mepc = mepc;
