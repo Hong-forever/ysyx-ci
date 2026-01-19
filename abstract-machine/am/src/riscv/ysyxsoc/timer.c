2c6286e91761ec83c017f772b801da0f3fd7de46
tracer-ysyx <tracer@ysyx.org>
2026-01-19 22:25:58 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:58 up 22:55,  1 user,  load average: 1.18, 1.41, 1.28

--- abstract-machine/am/src/riscv/ysyxsoc/timer.c
+++ abstract-machine/am/src/riscv/ysyxsoc/timer.c
@@ -1,6 +1,6 @@
 #include <ysyxsoc.h>
 
-#define NPC_PERIOD 9 // in us
+#define YSYXSOC_PERIOD 9 // in us
 
 static uint64_t us = 0;
 
@@ -9,7 +9,7 @@ void __am_timer_init()
     // uint32_t time_hi = inl(CLINT_PORT+ 4);
     // uint32_t time_lo = inl(CLINT_PORT);
 
-    // us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * NPC_PERIOD;
+    // us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * YSYXSOC_PERIOD;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
@@ -17,16 +17,16 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
     uint32_t time_hi = inl(CLINT_PORT+ 4);
     uint32_t time_lo = inl(CLINT_PORT);
 
-    uptime->us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * NPC_PERIOD;
+    uptime->us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * YSYXSOC_PERIOD;
 }
 
-void __am_timer_rtc(AM_TIMER_CLINT_T *rtc)
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
 {
     uint32_t time_hi = inl(CLINT_PORT+ 4);
     uint32_t time_lo = inl(CLINT_PORT);
 
     // ysyxsoc's period is 10us
-    uint64_t up_us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * NPC_PERIOD; 
+    uint64_t up_us = (((uint64_t)time_hi << 32) | (uint64_t)time_lo) * YSYXSOC_PERIOD; 
 
     uint64_t s = (us + up_us) / 1000000ULL;
     uint64_t day = s / 86400ULL;
