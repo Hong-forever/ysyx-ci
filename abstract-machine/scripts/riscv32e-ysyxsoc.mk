fc997217b5a2de2bf22bd198e0601108733f9d6f
tracer-ysyx <tracer@ysyx.org>
2026-01-20 09:09:46 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:46 up 1 day,  1:31,  1 user,  load average: 1.16, 1.06, 0.57

--- abstract-machine/scripts/riscv32e-ysyxsoc.mk
+++ abstract-machine/scripts/riscv32e-ysyxsoc.mk
@@ -1,11 +1,5 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/ysyxsoc.mk
-
-export PATH := $(PATH):$(abspath $(AM_HOME)/tools/minirv)
-CC = minirv-gcc
-AS = minirv-gcc
-CXX = minirv-g++
-
 COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
