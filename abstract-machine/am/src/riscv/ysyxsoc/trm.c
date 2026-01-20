1d97746b5acaa4a18ba4eba904500bcc3fbb9315
tracer-ysyx <tracer@ysyx.org>
2026-01-20 22:05:32 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:05:32 up 1 day, 14:27,  1 user,  load average: 1.09, 0.78, 0.47

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -18,8 +18,17 @@ void halt(int code)
         ;
 }
 
+static void serial_init()
+{
+    outb(SERIAL_PORT + 3, 0x80); // Enable DLAB
+    outb(SERIAL_PORT + 1, 0x00); // Set baud rate divisor to 1 (115200 baud)
+    outb(SERIAL_PORT + 0, 50000000 / 16 / 115200); // Divisor low byte
+    outb(SERIAL_PORT + 3, 0x03); // 8 bits, no parity, one stop bit
+}
+
 void _trm_init()
 {
+    serial_init();
     // uint32_t ysyx_logo = ysyxsoc_csr_read(CSR_MVENDORID);
     // // printf("mvendorid: 0x%08x\n", ysyx_logo);
     // uint32_t stu_num = ysyxsoc_csr_read(CSR_MARCHID);
