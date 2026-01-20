1244e70031abe3aa204c8a34f51b34ecd02b60a0
tracer-ysyx <tracer@ysyx.org>
2026-01-20 12:07:27 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:27 up 1 day,  4:29,  1 user,  load average: 1.19, 0.98, 0.80

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -36,8 +36,11 @@ static void report_mmio_overlap(const char *name1, paddr_t l1, paddr_t r1,
 void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
   paddr_t left = addr, right = addr + len - 1;
-  if (in_pmem(left) || in_pmem(right)) {
-    report_mmio_overlap(name, left, right, "pmem", PMEM_LEFT, PMEM_RIGHT);
+  if (in_rom(left) || in_rom(right)) {
+    report_mmio_overlap(name, left, right, "rom", PMEM_LEFT_ROM, PMEM_RIGHT_ROM);
+  }
+  if (in_ram(left) || in_ram(right)) {
+    report_mmio_overlap(name, left, right, "ram", PMEM_LEFT_RAM, PMEM_RIGHT_RAM);
   }
   for (int i = 0; i < nr_map; i++) {
     if (left <= maps[i].high && right >= maps[i].low) {
