d72889f4c5739e050a041062ee16cff40828a3af
tracer-ysyx <tracer@ysyx.org>
2026-01-21 18:18:45 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:18:45 up 2 days,  2:27,  1 user,  load average: 1.16, 0.92, 0.56

--- my_test/soc_test/bitrev_test/bitrev-test.c
+++ my_test/soc_test/bitrev_test/bitrev-test.c
@@ -2,21 +2,21 @@
 #include <klib.h>
 #include <klib-macros.h>
 
-#define SPI_FLASH_BASE  0x10001000
+#define SPI_MASTER_BASE  0x10001000
 #define SPI_TRX0_OFFSET 0x00
 #define SPI_CTRL_OFFSET 0x10
 #define SPI_DIV_OFFSET  0x14
 #define SPI_SS_OFFSET   0x18
 
 void spi_reg_write(uint32_t offset, uint32_t value) {
-    *(volatile uint32_t *)(SPI_FLASH_BASE + offset) = value;
+    *(volatile uint32_t *)(SPI_MASTER_BASE + offset) = value;
 }
 
 uint32_t spi_reg_read(uint32_t offset) {
-    return *(volatile uint32_t *)(SPI_FLASH_BASE + offset);
+    return *(volatile uint32_t *)(SPI_MASTER_BASE + offset);
 }
 
-void spi_tranfer(uint32_t value) {
+void spi_transfer(uint32_t value) {
     spi_reg_write(SPI_CTRL_OFFSET, spi_reg_read(SPI_CTRL_OFFSET) & ~0x100);
     spi_reg_write(SPI_TRX0_OFFSET, value);
     spi_reg_write(SPI_CTRL_OFFSET, spi_reg_read(SPI_CTRL_OFFSET) | 0x100);
@@ -26,29 +26,30 @@ uint32_t spi_receive() {
     return spi_reg_read(SPI_TRX0_OFFSET);
 }
 
-void spi_flash_init() {
+void spi_bitrev_init() {
     spi_reg_write(SPI_CTRL_OFFSET, 0x00); //reset
     spi_reg_write(SPI_DIV_OFFSET, 0x04);  //set clk divider
-    spi_reg_write(SPI_SS_OFFSET, 1 << 7);   //set ss 
+    spi_reg_write(SPI_SS_OFFSET, 1 << 7);   //set ss, select bitrev
     spi_reg_write(SPI_CTRL_OFFSET, 0x2410); //ass, char_len=16, txneg
 }
 
-void spi_tranfer_wait() {
+void spi_transfer_wait() {
     while(spi_reg_read(SPI_CTRL_OFFSET) & 0x100);
 }
 
 
 int main(const char *args) {
-    spi_flash_init();
+    spi_bitrev_init();
 
-    spi_tranfer(0x1200);
-    spi_tranfer_wait();
+    spi_transfer(0x1200);
+    spi_transfer_wait();
     uint32_t received = spi_receive();
     printf("Received: 0x%08x\n", received & 0x00ff);
     if((received & 0x00ff) == 0x48) {
         putstr("pass!\n");
     } else {
         putstr("fail!\n");
+        return 1;
     }
 
     return 0;
---
+++ my_test/soc_test/flash_test/flash-test.c
@@ -0,0 +1,54 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+#define SPI_MASTER_BASE  0x10001000
+#define SPI_TRX0_OFFSET 0x00
+#define SPI_CTRL_OFFSET 0x10
+#define SPI_DIV_OFFSET  0x14
+#define SPI_SS_OFFSET   0x18
+
+void spi_reg_write(uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(SPI_MASTER_BASE + offset) = value;
+}
+
+uint32_t spi_reg_read(uint32_t offset) {
+    return *(volatile uint32_t *)(SPI_MASTER_BASE + offset);
+}
+
+void spi_transfer(uint32_t value) {
+    spi_reg_write(SPI_CTRL_OFFSET, spi_reg_read(SPI_CTRL_OFFSET) & ~0x100);
+    spi_reg_write(SPI_TRX0_OFFSET, value);
+    spi_reg_write(SPI_CTRL_OFFSET, spi_reg_read(SPI_CTRL_OFFSET) | 0x100);
+}
+
+uint32_t spi_receive() {
+    return spi_reg_read(SPI_TRX0_OFFSET);
+}
+
+void spi_flash_init() {
+    spi_reg_write(SPI_CTRL_OFFSET, 0x00); //reset
+    spi_reg_write(SPI_DIV_OFFSET, 0x04);  //set clk divider
+    spi_reg_write(SPI_SS_OFFSET, 0);   //set ss, select flash 
+    spi_reg_write(SPI_CTRL_OFFSET, 0x2440); //ass, char_len=64, txneg
+}
+
+void spi_transfer_wait() {
+    while(spi_reg_read(SPI_CTRL_OFFSET) & 0x100);
+}
+
+uint32_t flash_read(uint32_t addr) {
+    spi_flash_init();
+    spi_transfer(0x03); //read flash cmd
+    spi_transfer_wait();
+    return spi_receive();
+}
+
+int main(const char *args) {
+    uint32_t data;
+    for(int i=0; i<10; i++) {
+        data = flash_read(i * 4);
+        printf("Data at address 0x%08x: %d\n", i * 4, data);
+    }
+    return 0;
+}
