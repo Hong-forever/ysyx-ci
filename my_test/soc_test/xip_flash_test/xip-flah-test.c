109c31cbae3e0419569216c4b5d8877ff67e82f6
tracer-ysyx <tracer@ysyx.org>
2026-01-22 11:32:55 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:55 up 2 days, 11:35,  1 user,  load average: 1.32, 1.14, 1.16

--- my_test/soc_test/xip_flash_test/xip-flah-test.c
+++ my_test/soc_test/xip_flash_test/xip-flah-test.c
@@ -6,11 +6,12 @@
 
 int main(const char *args) {
 
-    volatile uint32_t *p = (volatile uint32_t *)0x30000000;
-    for (int i = 0; i < SIZE / 4; i++) {
-        printf("Data at address 0x%08x: 0x%08x\n", (uint32_t)(0x30000000 + i * 4), p[i]);
-    }
-    p[0] = 0;
+    // volatile uint32_t *p = (volatile uint32_t *)0x30000000;
+    // for (int i = 0; i < SIZE / 4; i++) {
+    //     printf("Data at address 0x%08x: 0x%08x\n", (uint32_t)(0x30000000 + i * 4), p[i]);
+    // }
 
+    // test jump to flash
+    asm volatile("li t0, %0; jalr zero, t0, 0" : : "i"(0x30000000));
     return 0;
 }
