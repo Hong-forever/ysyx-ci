9cc374b6eeba551ed14902fb30562ca13f5c778a
tracer-ysyx <tracer@ysyx.org>
2026-01-22 14:59:06 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:06 up 2 days, 15:01,  1 user,  load average: 1.30, 0.84, 0.55

--- ysyx_npc/csrc/memory/paddr.c
+++ ysyx_npc/csrc/memory/paddr.c
@@ -128,7 +128,7 @@ extern "C" void paddr_write(paddr_t waddr, word_t wdata, uint32_t wmask) {
 
 extern "C" void flash_read(int32_t addr, int32_t *data) {
     printf("flash_read addr: 0x%08x\n", addr);
-    *data = pmem_read(addr);
+    *data = pmem_read(addr + CONFIG_MBASE);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) 
 {
