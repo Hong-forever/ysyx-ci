aa6d73aed65240d0c48f324934135fe568db0e08
tracer-ysyx <tracer@ysyx.org>
2026-01-23 19:24:39 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:39 up 17:02,  1 user,  load average: 1.42, 0.67, 0.37

--- my_test/soc_test/psram_test/psram_test.c
+++ my_test/soc_test/psram_test/psram_test.c
@@ -4,17 +4,12 @@
 
 int main(const char *args) {
 
-    volatile uint8_t *p = (volatile uint8_t *)0x80000000;
+    volatile uint16_t *p = (volatile uint16_t *)0x80000000;
 
-    p[0] = 0x12;
-    p[1] = 0x34;
+    p[0] = 0x1234;
 
-    if(p[0] != 0x12) {
+    if(p[0] != 0x1234) {
         putstr("psram_test failed at addr 0x80000000\n");
-        return -1;
-    }
-    if(p[1] != 0x34) {
-        putstr("psram_test failed at addr 0x80000001\n");
         return -1;
     }
 
