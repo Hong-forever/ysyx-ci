9964f51913a1f34e328fb3864e60b27a47ecbfca
tracer-ysyx <tracer@ysyx.org>
2026-01-24 18:59:33 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:33 up 1 day,  9:09,  1 user,  load average: 1.81, 2.61, 2.68

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -8,50 +8,50 @@ int main(const char *args) {
     uint8_t *p = (uint8_t *)malloc(SIZE);
     for (int i = 0; i < SIZE; i++) {
         p[i] = i % 256;
-        putstr("8 ");
+        // putstr("8 ");
     }
     for (int i = 0; i < SIZE; i++) {
         if (p[i] != i % 256) {
             putstr("error\n");
             return 1;
         }
-        putstr("\b\b");
+        // putstr("\b\b");
     }
     uint16_t *q = (uint16_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 2; i++) {
         q[i] = i % 65536;
-        putstr("16 ");
+        // putstr("16 ");
     }
     for (int i = 0; i < SIZE / 2; i++) {
         if (q[i] != i % 65536) {
             putstr("error\n");
             return 1;
         }
-        putstr("\b\b\b");
+        // putstr("\b\b\b");
     }
     uint32_t *r = (uint32_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 4; i++) {
         r[i] = i;
-        putstr("32 ");
+        // putstr("32 ");
     }
     for (int i = 0; i < SIZE / 4; i++) {
         if (r[i] != i) {
             putstr("error\n");
             return 1;
         }
-        putstr("\b\b\b");
+        // putstr("\b\b\b");
     }
     uint64_t *s = (uint64_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 8; i++) {
         s[i] = i;
-        putstr("64 ");
+        // putstr("64 ");
     }
     for (int i = 0; i < SIZE / 8; i++) {
         if (s[i] != i) {
             putstr("error\n");
             return 1;
         }
-        putstr("\b\b\b");
+        // putstr("\b\b\b");
     }
     putstr("mem test pass!\n");
     return 0;
