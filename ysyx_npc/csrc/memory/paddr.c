9ad44b25dc2ce6de5e0a9101a119af9a529dc60f
tracer-ysyx <tracer@ysyx.org>
2026-01-24 09:50:13 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:50:13 up 23:59,  1 user,  load average: 1.91, 1.55, 1.38

--- ysyx_npc/csrc/memory/paddr.c
+++ ysyx_npc/csrc/memory/paddr.c
@@ -2,40 +2,32 @@
 #include "utils.h"
 #include "device.h"
 
-static uint8_t pmem[CONFIG_MSIZE] = {0};
+static uint8_t* pmem = NULL;
 static uint32_t rtc_value[2] = {0};
 
-static inline bool in_pmem(paddr_t addr) {
-  return (addr - CONFIG_MBASE) < CONFIG_MSIZE;
-}
-
 uint8_t* guest_to_host(paddr_t paddr) {
-    return pmem + (paddr - CONFIG_MBASE);
+  if(in_mrom(paddr))  return pmem + paddr - CONFIG_MROM_BASE;
+  else if(in_flash(paddr))  return pmem + paddr - CONFIG_FLASH_BASE + CONFIG_MROM_SIZE;
+  else if(in_psram(paddr))  return pmem + paddr - CONFIG_PSRAM_BASE + CONFIG_MROM_SIZE + CONFIG_FLASH_SIZE;
+  else return NULL;
 }
-
-paddr_t host_to_guest(uint8_t *haddr) {
-    return (haddr - pmem) + CONFIG_MBASE;
+paddr_t host_to_guest(uint8_t *haddr) { 
+  if((haddr-pmem) < CONFIG_MROM_SIZE) return haddr - pmem + CONFIG_MROM_BASE;
+  else if((haddr-pmem) < (CONFIG_MROM_SIZE + CONFIG_FLASH_SIZE)) return haddr - pmem - CONFIG_MROM_SIZE + CONFIG_FLASH_BASE;
+  else if((haddr-pmem) < (CONFIG_MROM_SIZE + CONFIG_FLASH_SIZE + CONFIG_PSRAM_SIZE)) return haddr - pmem - CONFIG_MROM_SIZE - CONFIG_FLASH_SIZE + CONFIG_PSRAM_BASE;
+  else return 0;
 }
 
 static void out_of_bound(paddr_t addr, bool is_write) {
-    PRINTF_RED("%s address = 0x%08x is out of bound of pmem [0x%08x, 0x%08x] with the size of 0x%08x\n", is_write?"Write":"Read", addr, PMEM_LEFT, PMEM_RIGHT, CONFIG_MSIZE);
-    // assert(0);
-}
-
-static word_t host_read(uint8_t *addr) {
-    return *(word_t *)addr;
-}
-
-static void host_write(uint8_t *addr, word_t wdata, uint32_t wmask) {
-    if(wmask & 0x1)      addr[0] = wdata & 0xff;
-    if(wmask & 0x2)      addr[1] = (wdata >> 8) & 0xff;
-    if(wmask & 0x4)      addr[2] = (wdata >> 16) & 0xff;
-    if(wmask & 0x8)      addr[3] = (wdata >> 24) & 0xff;
+    PRINTF_RED("%s address = 0x%08x is out of bound of mrom [0x%08x, 0x%08x], flash [0x%08x, 0x%08x], psram [0x%08x, 0x%08x]\n", is_write?"Write":"Read", addr, PMEM_LEFT_MROM, PMEM_RIGHT_MROM, PMEM_LEFT_FLASH, PMEM_RIGHT_FLASH, PMEM_LEFT_PSRAM, PMEM_RIGHT_PSRAM);
+    assert(0);
 }
 
 void init_mem() {
-    IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE * sizeof(uint8_t)));
-    PRINTF_BLUE("physical memory area [0x%08x, 0x%08x]\n", PMEM_LEFT, PMEM_RIGHT);
+    pmem = (uint8_t *)malloc(CONFIG_MROM_SIZE + CONFIG_FLASH_SIZE + CONFIG_PSRAM_SIZE);
+    assert(pmem);
+    IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), (CONFIG_MROM_SIZE + CONFIG_FLASH_SIZE + CONFIG_PSRAM_SIZE) * sizeof(uint8_t)));
+    PRINTF_BLUE("physical memory area mrom [0x%08x, 0x%08x], sram(No trace) [0x%08x, 0x%08x], flash [0x%08x, 0x%08x], psram [0x%08x, 0x%08x]\n", PMEM_LEFT_MROM, PMEM_RIGHT_MROM, PMEM_LEFT_SRAM, PMEM_RIGHT_SRAM, PMEM_LEFT_FLASH, PMEM_RIGHT_FLASH, PMEM_LEFT_PSRAM, PMEM_RIGHT_PSRAM);
 }
 
 IFDEF(MTRACE,
@@ -62,97 +54,61 @@ static word_t pmem_read(paddr_t raddr)
     return ret;
 }
 
-static void pmem_write(paddr_t waddr, word_t wdata, uint32_t wmask)
+static void pmem_write(paddr_t waddr, word_t wdata, uint32_t len)
 {
-    // printf("waddr: 0x%08x\nwdata: 0x%08x\nmask:0x%08x\n", waddr, wdata, tra_mask(wmask));
-    host_write(guest_to_host(waddr), wdata, wmask);
-    IFDEF(MTRACE, mtrace_write(waddr, wdata, wmask));
+    // printf("waddr: 0x%08x\nwdata: 0x%08x\nmask:0x%x\n", waddr, wdata, len);
+    host_write(guest_to_host(waddr), wdata, len);
+    IFDEF(MTRACE, mtrace_write(waddr, wdata, len));
 }
 
-extern "C" word_t paddr_read(paddr_t raddr) {
-    if (in_pmem(raddr)) {
-        // printf("paddr_read addr: 0x%08x\n", raddr);
-        return pmem_read(raddr);
-    } 
-    else {
-        if ((raddr & ~0x7u) == RTC_MMIO) {
-            if (raddr & 0x4) {
-                uint64_t us = get_time();
-                if (rtc_value[0] == 0 && rtc_value[1] == 0) {
-                    rtc_value[0] = boot_time & 0xffffffff;
-                    rtc_value[1] = (boot_time >> 32) & 0xffffffff;
-                    return rtc_value[1];
-                } else {
-                    rtc_value[0] = us & 0xffffffff;
-                    rtc_value[1] = (us >> 32) & 0xffffffff;
-                    return rtc_value[1];
-                }
-            } else {
-                return rtc_value[0];
-            }
-        }
-
-        out_of_bound(raddr, false);
+word_t paddr_read(paddr_t addr) {
+    if (in_mrom(addr) || in_flash(addr) || in_psram(addr)) {
+        return pmem_read(addr);
+    } else {
+        out_of_bound(addr, false);
         return 0;
     }
 }
 
-extern "C" void paddr_write(paddr_t waddr, word_t wdata, uint32_t wmask) {
-    if (in_pmem(waddr)) {
-        pmem_write(waddr, wdata, wmask);
-    }
-    else {
-        if ((waddr & ~0x3u) == SERIAL_MMIO) {
-            // memory-mapped serial port write
-            assert(wmask == 0x1);
-            putc((char)(wdata & 0xff), stderr);
-        } else {
-            out_of_bound(waddr, true);
-        }
+void paddr_write(paddr_t addr, word_t data, int len) {
+    if (in_flash(addr) || in_mrom(addr)) {
+        PRINTF_RED("Cannot write to mrom or flash address 0x%08x\n", addr);
+        assert(0);
+    } else if (in_psram(addr)) {
+        pmem_write(addr, data, len);
+    } else {
+        out_of_bound(addr, true);
     }
 }
 
-// static word_t flash_mem[CONFIG_MSIZE] = {0x100007b7, 0x04100713, 0x00e78023, 0x00100073, 0x0000006f}; // dummy flash memory
-// static word_t flash_mem[CONFIG_MSIZE] = {0x12345678, 0x13579135, 0x24682468, 0x87654321}; // dummy flash memory
-
-extern "C" void flash_read(int32_t addr, int32_t *data) {
-    // printf("flash_read addr: 0x%08x\n", addr);
-    *data = pmem_read(addr + CONFIG_MBASE);
-}
 extern "C" void mrom_read(int32_t addr, int32_t *data) 
 {
     // printf("mrom_read addr: 0x%08x\n", addr);
     *data = pmem_read(addr);
 }
 
-static uint8_t psram[2000] = {0};
+extern "C" void flash_read(int32_t addr, int32_t *data) {
+    // printf("flash_read addr: 0x%08x\n", addr);
+    //spi
+    *data = pmem_read(addr + CONFIG_FLASH_BASE);
+}
 
 extern "C" void psram_read(int32_t addr, int32_t *data) {
-    *data = psram[addr];
+    //spi
+    *data = pmem_read(addr + CONFIG_PSRAM_BASE);
     // printf("psram_read addr: 0x%08x, data: 0x%02x\n", addr, *data);
 }
 
 extern "C" void psram_write(int32_t addr, int32_t data, int32_t len) {
     // printf("psram_write addr: 0x%08x data: 0x%08x len: %d\n", addr, data, len);
     if(len == 1) {
-        psram[addr] = data;
-        // printf("psram[%x] = 0x%02x\n", addr, psram[addr]);
+        pmem_write(addr + CONFIG_PSRAM_BASE, data, 0x1);
     }
     if(len == 2) {
-        psram[addr] = data;
-        psram[addr+1] = data >> 8;
-        // printf("psram[%x] = 0x%02x\n", addr, psram[addr]);
-        // printf("psram[%x] = 0x%02x\n", addr+1, psram[addr+1]);
+        pmem_write(addr + CONFIG_PSRAM_BASE, data, 0x3);
     }
     if(len == 4) {
-        psram[addr] = data;
-        psram[addr+1] = data >> 8;
-        psram[addr+2] = data >> 16;
-        psram[addr+3] = data >> 24;
-        // printf("psram[%x] = 0x%02x\n", addr, psram[addr]);
-        // printf("psram[%x] = 0x%02x\n", addr+1, psram[addr+1]);
-        // printf("psram[%x] = 0x%02x\n", addr+2, psram[addr+2]);
-        // printf("psram[%x] = 0x%02x\n", addr+3, psram[addr+3]);
+        pmem_write(addr + CONFIG_PSRAM_BASE, data, 0xf);
     }
 
 }
