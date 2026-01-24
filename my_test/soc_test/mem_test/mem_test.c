526df8468fd0ad7513ce4a0dbdb3f7f974b029e8
tracer-ysyx <tracer@ysyx.org>
2026-01-24 20:47:35 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:35 up 1 day, 10:57,  1 user,  load average: 1.46, 1.16, 1.05

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -7,43 +7,43 @@
 extern Area heap;
 
 int main(const char *args) {
-    uint8_t *p8 = (uint8_t *)heap.start;
+    // uint8_t *p8 = (uint8_t *)heap.start;
 
-    for (int i = 0; &p8[i] < (uint8_t *)heap.end; p8++, i++) {
-        p8[i] = i % 256;
-        if(p8[i] != i % 256) {
-            putstr("error\n");
-            return 1;
-        }
-    }
+    // for (int i = 0; &p8[i] < (uint8_t *)heap.end; i++) {
+    //     p8[i] = i % 256;
+    //     if(p8[i] != i % 256) {
+    //         putstr("error\n");
+    //         return 1;
+    //     }
+    // }
 
-    putstr("8 ");
+    // putstr("8 ");
 
-    uint16_t *p16 = (uint16_t *)heap.start;
+    // uint16_t *p16 = (uint16_t *)heap.start;
 
-    for (int i = 0; &p16[i] < (uint16_t *)heap.end; p16++, i++) {
-        p16[i] = i % 65536;
-        if(p16[i] != i % 65536) {
-            putstr("error\n");
-            return 1;
-        }
-    }
+    // for (int i = 0; &p16[i] < (uint16_t *)heap.end; i++) {
+    //     p16[i] = i % 65536;
+    //     if(p16[i] != i % 65536) {
+    //         putstr("error\n");
+    //         return 1;
+    //     }
+    // }
 
-    putstr("16 ");
+    // putstr("16 ");
 
-    uint32_t *p32 = (uint32_t *)heap.start;
-    for (int i = 0; &p32[i] < (uint32_t *)heap.end; p32++, i++) {
-        p32[i] = i;
-        if(p32[i] != i) {
-            putstr("error\n");
-            return 1;
-        }
-    }
+    // uint32_t *p32 = (uint32_t *)heap.start;
+    // for (int i = 0; &p32[i] < (uint32_t *)heap.end; i++) {
+    //     p32[i] = i;
+    //     if(p32[i] != i) {
+    //         putstr("error\n");
+    //         return 1;
+    //     }
+    // }
 
-    putstr("32 ");
+    // putstr("32 ");
 
     uint64_t *p64 = (uint64_t *)heap.start;
-    for (int i = 0; &p64[i] < (uint64_t *)heap.end; p64++, i++) {
+    for (int i = 0; &p64[i] < (uint64_t *)heap.end; i++) {
         p64[i] = i;
         if(p64[i] != i) {
             putstr("error\n");
