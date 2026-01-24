1e80f523baf8c3093320a6698c5e8a5999d3b5cc
tracer-ysyx <tracer@ysyx.org>
2026-01-24 21:03:58 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:58 up 1 day, 11:13,  1 user,  load average: 1.67, 1.30, 1.26

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -42,7 +42,7 @@ int main(const char *args) {
 
     // putstr("32 ");
 
-    uint64_t *p64 = (uint64_t *)heap.end -1;
+    uint64_t *p64 = (uint64_t *)heap.end - 20;
     for (int i = 0; &p64[i] < (uint64_t *)heap.end; i++) {
         p64[i] = i;
         if(p64[i] != i) {
