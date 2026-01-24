021ccdab4e1102d64d4b3ca2de963f36c7189930
tracer-ysyx <tracer@ysyx.org>
2026-01-24 18:04:45 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:44 up 1 day,  8:14,  1 user,  load average: 1.62, 1.07, 0.89

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -2,7 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 
-#define SIZE 1024
+#define SIZE 1024 * 1024 * 4
 
 int main(const char *args) {
     uint8_t *p = (uint8_t *)malloc(SIZE);
@@ -19,11 +19,11 @@ int main(const char *args) {
     }
     uint16_t *q = (uint16_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 2; i++) {
-        q[i] = i * 2;
+        q[i] = i % 65536;
         putstr("16 ");
     }
     for (int i = 0; i < SIZE / 2; i++) {
-        if (q[i] != i * 2) {
+        if (q[i] != i % 65536) {
             putstr("error\n");
             return 1;
         }
@@ -31,11 +31,11 @@ int main(const char *args) {
     }
     uint32_t *r = (uint32_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 4; i++) {
-        r[i] = i * 3;
+        r[i] = i;
         putstr("32 ");
     }
     for (int i = 0; i < SIZE / 4; i++) {
-        if (r[i] != i * 3) {
+        if (r[i] != i) {
             putstr("error\n");
             return 1;
         }
@@ -43,11 +43,11 @@ int main(const char *args) {
     }
     uint64_t *s = (uint64_t *)malloc(SIZE);
     for (int i = 0; i < SIZE / 8; i++) {
-        s[i] = i * 4;
+        s[i] = i;
         putstr("64 ");
     }
     for (int i = 0; i < SIZE / 8; i++) {
-        if (s[i] != i * 4) {
+        if (s[i] != i) {
             putstr("error\n");
             return 1;
         }
