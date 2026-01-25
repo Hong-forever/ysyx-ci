a9bb323c92b1bb7f56c116b29cd342e322e4357c
tracer-ysyx <tracer@ysyx.org>
2026-01-25 09:56:57 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:56:57 up 1 day, 16:28,  1 user,  load average: 1.25, 0.91, 0.68

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,9 +1,9 @@
 #include <ysyxsoc.h>
 
-extern char _sheap, _eheap;
+extern char _heap_start, _heap_end;
 int main(const char *args);
 
-Area heap = RANGE(&_sheap, &_eheap);
+Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 // divisor factor = freq / (16 * baudrate)
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -37,9 +37,9 @@ void *malloc(size_t size) {
     //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 
     static unsigned char *addr = NULL; 
-    extern char _sheap;
+    extern char _heap_start;
     /* printf("_heap: %x\n", &_heap_start); */
-    if(addr == NULL) addr = (unsigned char *)&_sheap;
+    if(addr == NULL) addr = (unsigned char *)&_heap_start;
 
     if(size == 0) return NULL;
 
