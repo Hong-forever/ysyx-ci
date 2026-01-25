eb96f6a26f9e67b13ea2b02ddd1f3d0aa800595c
tracer-ysyx <tracer@ysyx.org>
2026-01-25 11:32:17 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:17 up 1 day, 18:04,  1 user,  load average: 1.01, 0.79, 0.65

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -3,7 +3,9 @@
 extern char _heap_start, _heap_end;
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, &_heap_end);
+extern char  __fsymtab_start;
+
+Area heap = RANGE(&_heap_start, &__fsymtab_start);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 // divisor factor = freq / (16 * baudrate)
