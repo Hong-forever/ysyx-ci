b68e481740cde4339b34e742bb8aad0ac407d0c8
tracer-ysyx <tracer@ysyx.org>
2026-01-26 12:01:06 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:06 up  5:46,  1 user,  load average: 0.76, 0.38, 0.46

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -35,11 +35,11 @@
 #define FB_ADDR         (MMIO_BASE   + 0x1000000)
 #define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
 
-extern char _rom_start, _rom_end;
+extern char _rom_aaastart, _rom_end;
 extern char _ram_start, _ram_end;
 
 #define NEMU_PADDR_SPACE \
-  RANGE(&_rom_start, &_rom_end), \
+  RANGE(&_rom_aaastart, &_rom_end), \
   RANGE(&_ram_start, &_ram_end), \
   RANGE(FB_ADDR, FB_ADDR + 0x200000), \
   RANGE(MMIO_BASE, MMIO_BASE + 0x1000) /* serial, rtc, screen, keyboard */
