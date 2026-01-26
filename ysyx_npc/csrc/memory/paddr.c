e0a88e83aeefe36887905d0d3495ce3250462d84
tracer-ysyx <tracer@ysyx.org>
2026-01-26 16:03:50 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:50 up  9:49,  1 user,  load average: 1.15, 0.92, 0.67

--- ysyx_npc/csrc/memory/paddr.c
+++ ysyx_npc/csrc/memory/paddr.c
@@ -124,7 +124,7 @@ extern "C" void sdram_read(int32_t addr, int32_t *data) {
 }
 
 extern "C" void sdram_write(int32_t addr, int32_t data, int32_t mask) {
-    // printf("sdram_write addr: 0x%08x data: 0x%08x mask: %d\n", addr, data, mask);
+    printf("sdram_write addr: 0x%08x data: 0x%02x mask: %02x\n", addr, data, ~mask);
     switch ((~mask) & 0x3)
     {
         case 0:
