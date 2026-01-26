c54c07ac96680da1c25927f316e6bde4fa2789b1
tracer-ysyx <tracer@ysyx.org>
2026-01-26 14:45:56 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:56 up  8:31,  1 user,  load average: 2.20, 1.79, 1.19

--- ysyx_npc/vsrc/ifetch.v
+++ ysyx_npc/vsrc/ifetch.v
@@ -110,7 +110,8 @@ module ysyx_25110270_ifetch
                 (ibus_araddr >= `MromAddrBase  && ibus_araddr <= (`MromAddrBase + `MromSize - 1))   || 
                 (ibus_araddr >= `SramAddrBase  && ibus_araddr <= (`SramAddrBase + `SramSize - 1))   ||
                 (ibus_araddr >= `FlashAddrBase && ibus_araddr <= (`FlashAddrBase + `FlashSize - 1)) ||
-                (ibus_araddr >= `PsramAddrBase && ibus_araddr <= (`PsramAddrBase + `PsramSize - 1))
+                (ibus_araddr >= `PsramAddrBase && ibus_araddr <= (`PsramAddrBase + `PsramSize - 1)) ||
+                (ibus_araddr >= `SdramAddrBase && ibus_araddr <= (`SdramAddrBase + `SdramSize - 1))
             )) begin
             pc <= 0;
             $error("IFETCH: PC address out of range at pc = 0x%08x", ibus_araddr);
