ebcb3e9f0a40f8d6aa01b70be9a62d1b9aab6e3d
tracer-ysyx <tracer@ysyx.org>
2026-01-27 12:48:48 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:48:48 up 23:06,  1 user,  load average: 2.17, 1.52, 1.12

--- ysyx_npc/csrc/memory/paddr.c
+++ ysyx_npc/csrc/memory/paddr.c
@@ -118,18 +118,16 @@ extern "C" void psram_write(int32_t addr, int32_t data, int32_t len) {
 
 extern "C" void sdram_read(int32_t addr, int32_t *data, int32_t num) {
 
-    printf("num: %d\n", num);
     uint32_t read_data = paddr_read(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * num);
     
     *data = addr&0x02 ? read_data >> 16 : 
                         read_data & 0xffff;
 
-    // printf("sdram_read addr: 0x%08x, data: 0x%02x\n", addr, read_data);
+    printf("sdram_read addr: 0x%08x, data: 0x%02x, num: %d\n", addr, read_data, num);
 }
 
 extern "C" void sdram_write(int32_t addr, int32_t data, int32_t mask, int32_t num) {
-    // printf("sdram_write addr: 0x%08x data: 0x%02x mask: %02x\n", addr, data, ~mask);
-    printf("num: %d\n", num);
+    printf("sdram_write addr: 0x%08x data: 0x%02x mask: %02x, num: %d\n", addr, data, ~mask, num);
     switch ((~mask) & 0x3)
     {
         case 0:
