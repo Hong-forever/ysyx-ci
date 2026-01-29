6861d0a504d87f74fe9bfebf2ac338b439b9a351
tracer-ysyx <tracer@ysyx.org>
2026-01-29 20:26:05 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:05 up 15:00,  1 user,  load average: 0.52, 0.54, 0.44

--- my_test/soc_test/nvboard_test/seg_test/seg_test.c
+++ my_test/soc_test/nvboard_test/seg_test/seg_test.c
@@ -10,7 +10,7 @@
 int main(const char *args) {
 
     volatile uint16_t *led = (volatile uint16_t *)LED_ADDR;
-    volatile uint16_t *sw = (volatile uint16_t *)SW_ADDR;
+    // volatile uint16_t *sw = (volatile uint16_t *)SW_ADDR;
     volatile uint32_t *seg = (volatile uint32_t *)SEG_ADDR;
 
     // while(*sw != 0x1234);
