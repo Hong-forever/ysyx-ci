b75760d54603fe99b0cef6fc91c6821994aaa597
tracer-ysyx <tracer@ysyx.org>
2026-01-29 21:38:38 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:38:38 up 16:12,  1 user,  load average: 0.26, 0.16, 0.28

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -10,8 +10,8 @@ static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); /
 static void serial_init()
 {
     outb(SERIAL_PORT + 3, 0x80); // Enable DLAB
-    outb(SERIAL_PORT + 1, 0x00); // Set baud rate divisor
-    outb(SERIAL_PORT + 0, 16); // Divisor low byte
+    outb(SERIAL_PORT + 1, 0x02); // Set baud rate divisor
+    outb(SERIAL_PORT + 0, 0x1e); // Divisor low byte
     outb(SERIAL_PORT + 3, 0x03); // 8 bits, no parity, one stop bit
 }
 
