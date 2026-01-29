fb506771200e5a5181aa8411507190bff24e416f
tracer-ysyx <tracer@ysyx.org>
2026-01-29 14:17:35 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:35 up  9:26,  1 user,  load average: 0.52, 0.73, 0.77

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -11,8 +11,8 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 CFLAGS    += -fdata-sections -ffunction-sections
 CFLAGS    += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
 LDSCRIPTS += $(AM_HOME)/scripts/soc_linker.ld
-MEM_LD    += $(AM_HOME)/scripts/memory.ld
-# LDFLAGS   += --defsym=_rom_start=0x20000000 --defsym=_entry_offset=0x0
+# LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
+# LDFLAGS   += --defsym=_pmem_start=0xa0000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 
 NPCFLAGS  += --log=$(shell dirname $(IMAGE).elf)/ysyxsoc-log.txt
