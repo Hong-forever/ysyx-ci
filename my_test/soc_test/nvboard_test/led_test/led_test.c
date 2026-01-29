cce1fd01a8bfc47c3115fbd58b1999713eb015f1
tracer-ysyx <tracer@ysyx.org>
2026-01-29 17:11:32 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:32 up 12:20,  1 user,  load average: 1.90, 1.82, 1.30

--- my_test/soc_test/nvboard_test/led_test/led_test.c
+++ my_test/soc_test/nvboard_test/led_test/led_test.c
@@ -2,10 +2,11 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+#define LED_ADDR 0x10002000
+
 int main(const char *args) {
 
-    volatile uint16_t *p = (volatile uint16_t *)0x10002000;
-
+    volatile uint16_t *p = (volatile uint16_t *)LED_ADDR;
     *p = 0xAAAA;
 
     while(1);
