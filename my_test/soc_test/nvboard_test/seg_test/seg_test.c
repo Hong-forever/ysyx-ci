e6a6ba0cd2feedb1c070d1fb23ef631495f8ada3
tracer-ysyx <tracer@ysyx.org>
2026-01-29 20:39:13 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:13 up 15:13,  1 user,  load average: 1.51, 1.23, 0.84

--- my_test/soc_test/nvboard_test/seg_test/seg_test.c
+++ my_test/soc_test/nvboard_test/seg_test/seg_test.c
@@ -22,7 +22,7 @@ int main(const char *args) {
         *led = 1 << i;
         i++;
         if(i >= 16) i = 0;
-        printf("i = %x\n", i);
+        // printf("i = %x\n", i);
         for (volatile int j = 0; j < 1000; j++);
         // while((*sw) != (1 << i));
     }
