b34f32b6446f86b2c290d6cbc0826f1a64fcaad0
tracer-ysyx <tracer@ysyx.org>
2026-01-29 23:14:45 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:45 up 17:48,  1 user,  load average: 1.06, 1.61, 1.83

--- my_test/soc_test/nvboard_test/seg_test/seg_test.c
+++ my_test/soc_test/nvboard_test/seg_test/seg_test.c
@@ -23,7 +23,7 @@ int main(const char *args) {
         i++;
         if(i >= 16) i = 0;
         putstr("LED changed\n");
-        for (volatile int j = 0; j < 100; j++); 
+        for (volatile int j = 0; j < 1000; j++); 
         // while((*sw) != (1 << i));
     }
     
