b8d8cd5aa665085e09ead64b69d3a12ffc321bfa
tracer-ysyx <tracer@ysyx.org>
2026-01-30 12:00:34 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:00:34 up 22:57,  1 user,  load average: 1.91, 1.72, 1.27

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -118,7 +118,7 @@ extern "C" void psram_write(int32_t addr, int32_t data, int32_t len) {
 
 extern "C" void sdram_read(int32_t addr, int32_t *data, int32_t num) {
 
-    uint32_t read_data = paddr_read(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * num);
+    uint32_t read_data = paddr_read(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * (num >> 1));
     
     *data = addr&0x02 ? read_data >> 16 : 
                         read_data & 0xffff;
@@ -133,13 +133,13 @@ extern "C" void sdram_write(int32_t addr, int32_t data, int32_t mask, int32_t nu
         case 0:
             break;
         case 1:
-            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * num, data & 0xff, 1);
+            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * (num >> 1), data & 0xff, 1);
             break;
         case 2:
-            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * num + 1, (data & 0xff00) >> 8, 1);
+            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * (num >> 1) + 1, (data & 0xff00) >> 8, 1);
             break;
         case 3:
-            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * num, data & 0xffff, 2);
+            paddr_write(addr + CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE / 4 * (num >> 1), data & 0xffff, 2);
             break;
         default:
             break;
