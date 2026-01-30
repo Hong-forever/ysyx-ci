0cbfb99bf0ad4951a445b28234b8ff3a9ba540cd
tracer-ysyx <tracer@ysyx.org>
2026-01-30 17:09:33 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:33 up 1 day,  4:06,  1 user,  load average: 1.59, 1.15, 0.83

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -123,11 +123,11 @@ extern "C" void sdram_read(int32_t addr, int32_t *data, int32_t num) {
     *data = num & 0x1 ? read_data >> 16 : 
                         read_data & 0xffff;
 
-    if(addr >= 0x9dbd4 && addr < 0x9dbd8) printf("sdram_read addr: 0x%08x, data: 0x%02x, num: %d\n", addr, *data, num);
+    // if(addr >= 0x9dbd4 && addr < 0x9dbd8) printf("sdram_read addr: 0x%08x, data: 0x%02x, num: %d\n", addr, *data, num);
 }
 
 extern "C" void sdram_write(int32_t addr, int32_t data, int32_t mask, int32_t num) {
-    if(addr >= 0x9dbd4 && addr < 0x9dbd8) printf("sdram_write addr: 0x%08x data: 0x%02x mask: %02x, num: %d\n", addr, data, ~mask&0x3, num);
+    // if(addr >= 0x9dbd4 && addr < 0x9dbd8) printf("sdram_write addr: 0x%08x data: 0x%02x mask: %02x, num: %d\n", addr, data, ~mask&0x3, num);
     switch ((~mask) & 0x3)
     {
         case 0:
