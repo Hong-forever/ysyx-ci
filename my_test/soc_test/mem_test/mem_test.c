68e3d17411817fd81a7d07c566ec91f7eade42b9
tracer-ysyx <tracer@ysyx.org>
2026-01-31 15:26:57 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:26:57 up  1:23,  1 user,  load average: 1.32, 0.78, 1.14

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -3,59 +3,41 @@
 #include <klib-macros.h>
 
 #define BASE 0xa0000000
-#define SIZE 0x8000000
+#define SIZE 0x8000
+
+#define p_period 100
+
+#define PUT(i, period, bit, op) \
+    do { \
+        if(i % period == 0) printf("%s: %x(%d) ", op, i/period, bit); \
+    } while (0)
+
+#define TEST(bit, val_max, type) \
+    do { \
+        volatile type *p = (volatile type *)BASE; \
+        for (type i = 0; &p[i] < (volatile type *)(BASE + SIZE); i++) { \
+            p[i] = i % val_max; \
+            PUT(i, p_period, bit, "write"); \
+        } \
+        printf("\n"); \
+        for(type i = 0; &p[i] < (volatile type *)(BASE + SIZE); i++) { \
+            if(p[i] != i % val_max) { \
+                putstr("error\n"); \
+                return 1; \
+            } \
+            PUT(i, p_period, bit, "read"); \
+        } \
+        printf("\n"); \
+    } while (0)
 
 int main(const char *args) {
 
     putstr("mem test start!\n");
 
-    volatile uint8_t *p8 = (volatile uint8_t *)BASE;
-    for (int i = 0; &p8[i] < (volatile uint8_t *)(BASE + SIZE); i++) {
-        p8[i] = i % 256;
-        if(i % 1000 == 0) printf("%x(%d) ", i/1000, 8);
-    }
-    for(int i = 0; &p8[i] < (volatile uint8_t *)(BASE + SIZE); i++) {
-        if(p8[i] != i % 256) {
-            putstr("error\n");
-            return 1;
-        }
-    }
-
-    volatile uint16_t *p16 = (volatile uint16_t *)BASE;
-    for (int i = 0; &p16[i] < (volatile uint16_t *)(BASE + SIZE); i++) {
-        p16[i] = i % 65536;
-        if(i % 1000 == 0) printf("%x(%d) ", i/1000, 16);
-    }
-    for(int i = 0; &p16[i] < (volatile uint16_t *)(BASE + SIZE); i++) {
-        if(p16[i] != i % 65536) {
-            putstr("error\n");
-            return 1;
-        }
-    }
-
-    volatile uint32_t *p32 = (volatile uint32_t *)BASE;
-    for (int i = 0; &p32[i] < (volatile uint32_t *)(BASE + SIZE); i++) {
-        p32[i] = i;
-        if(i % 1000 == 0) printf("%x(%d) ", i/1000, 32);
-    }
-    for(int i = 0; &p32[i] < (volatile uint32_t *)(BASE + SIZE); i++) {
-        if(p32[i] != i) {
-            putstr("error\n");
-            return 1;
-        }
-    }
-
-    volatile uint64_t *p64 = (volatile uint64_t *)BASE;
-    for (int i = 0; &p64[i] < (volatile uint64_t *)(BASE + SIZE); i++) {
-        p64[i] = i;
-        if(i % 1000 == 0) printf("%x(%d) ", i/1000, 64);
-    }
-    for(int i = 0; &p64[i] < (volatile uint64_t *)(BASE + SIZE); i++) {
-        if(p64[i] != i) {
-            putstr("error\n");
-            return 1;
-        }
-    }
+    TEST(8,  256,  uint8_t );
+    TEST(16, 65536, uint16_t);
+    TEST(32, 1234321, uint32_t);
+    TEST(64, 987656789, uint64_t);
 
     putstr("mem test pass!\n");
     return 0;
