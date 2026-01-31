cefe89c77312173368d3afe87a962a5e5ce9ed71
tracer-ysyx <tracer@ysyx.org>
2026-01-31 16:45:36 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:36 up  2:41,  1 user,  load average: 1.80, 1.99, 1.84

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -9,7 +9,7 @@
 
 #define PUT(i, period, op, bit) \
     do { \
-        if(i % period == 0) printf("%s: 0x%x-0x%x ", op, i * bit / 8 * period, (i + 1) * bit / 8 * period - 1); \
+        if(i % period == 0) printf("%s: 0x%x-0x%x ", op, i * bit / 8, (i + 1) * bit / 8 - 1); \
     } while (0)
 
 #define TEST(bit, val_max, type) \
