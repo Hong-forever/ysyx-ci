1dd40ccc78b0ccbbb7cf980c7df2b356e03193ce
tracer-ysyx <tracer@ysyx.org>
2026-02-02 17:53:33 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:33 up 1 day, 20:09,  1 user,  load average: 2.45, 1.91, 1.73

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -245,12 +245,12 @@ module ysyx_25110270_lsu
         end
     end
 
-    parameter IDLE = 3'b001;
-    parameter MEM  = 3'b010;
-    parameter WB   = 3'b100;
+    parameter IDLE = 0;
+    parameter MEM  = 1;
+    parameter WB   = 2;
 
     reg data_avalid;
-    reg [2:0] state, nstate;
+    reg [1:0] state, nstate;
     always @(posedge clk) begin
         if(!rst_n) begin
             state <= IDLE;
