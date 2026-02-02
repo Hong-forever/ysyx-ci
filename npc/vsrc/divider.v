3a5f53300b0b5baa8bfc2213bfc7164389be9317
tracer-ysyx <tracer@ysyx.org>
2026-02-02 20:39:27 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:27 up 1 day, 22:55,  1 user,  load average: 2.09, 1.61, 1.39

--- npc/vsrc/divider.v
+++ npc/vsrc/divider.v
@@ -22,25 +22,23 @@ module ysyx_25110270_div
     parameter DivOn     = 2'b10;
     parameter DivEnd    = 2'b11;
 
-    wire [32:0] div_temp;
     reg [5:0] cnt;
     reg [64:0] dividend;
     reg [1:0] state;
     reg [31:0] divisor;
-    reg [31:0] temp_op1;
-    reg [31:0] temp_op2;
 
     reg [63:0] result;
     reg ready;
 
-    assign div_temp = {1'b0, dividend[63:32]} - {1'b0, divisor};
+    wire [32:0] div_temp = {1'b0, dividend[63:32]} - {1'b0, divisor};
+
+    wire [31:0] temp_op1 = (I_signed_div && I_opdata1[31]) ? (~I_opdata1 + 1) : I_opdata1;
+    wire [31:0] temp_op2 = (I_signed_div && I_opdata2[31]) ? (~I_opdata2 + 1) : I_opdata2;
 
     always @(posedge clk) begin
         if(!rst_n) begin
             state    <= DivFree;
             cnt      <= 0;
-            temp_op1 <= 0;
-            temp_op2 <= 0;
             dividend <= 0;
             divisor  <= 0;
             ready    <= 0;
@@ -48,26 +46,15 @@ module ysyx_25110270_div
         end else begin
             case(state)
                 DivFree: begin
-                    if(I_start == 1'b1 && I_annul == 1'b0) begin
+                    if(I_start && !I_annul) begin
                         if(I_opdata2 == 0) begin
                             state <= DivByZero;
                         end else begin
                             state <= DivOn;
                             cnt   <= 0;
-                            if(I_signed_div == 1'b1 && I_opdata1[31] == 1'b1 ) begin
-                                temp_op1 <= ~I_opdata1 + 1;
-                            end else begin
-                                temp_op1 <= I_opdata1;
-                            end
-                            if(I_signed_div == 1'b1 && I_opdata2[31] == 1'b1 ) begin
-                                temp_op2 <= ~I_opdata2 + 1;
-                            end else begin
-                                temp_op2 <= I_opdata2;
-                            end
                         end
-                        dividend       <= 0;
-                        dividend[32:1] <= temp_op1;
-                        divisor        <= temp_op2;
+                        dividend  <= {31'b0, temp_op1, 1'b0};
+                        divisor   <= temp_op2;
                     end else begin
                         ready <= 0;
                     end
@@ -83,23 +70,23 @@ module ysyx_25110270_div
                     state <= DivFree;
                 end
                 DivOn: begin
-                    if(I_annul == 1'b0) begin
-                        if(cnt != 6'b100000) begin
-                            if(div_temp[32] == 1'b1) begin
+                    if(!I_annul) begin
+                        if(cnt == 6'b100000) begin
+                            if(I_signed_div && (I_opdata1[31] ^ I_opdata2[31])) begin
+                                dividend[31:0] <= (~dividend[31:0] + 1);
+                            end
+                            if(I_signed_div && (I_opdata1[31] ^ dividend[64])) begin
+                                dividend[64:33] <= (~dividend[64:33] + 1);
+                            end
+                            state <= DivEnd;
+                            cnt <= 0;
+                        end else begin
+                            if(div_temp[32]) begin
                                 dividend <= {dividend[63:0], 1'b0};
                             end else begin
                                 dividend <= {div_temp[31:0], dividend[31:0], 1'b1};
                             end
                             cnt <= cnt + 1;
-                        end else begin
-                            if((I_signed_div == 1'b1) && ((I_opdata1[31] ^ I_opdata2[31]) == 1'b1)) begin
-                                dividend[31:0] <= (~dividend[31:0] + 1);
-                            end
-                            if((I_signed_div == 1'b1) && ((I_opdata1[31] ^ dividend[64]) == 1'b1)) begin
-                                dividend[64:33] <= (~dividend[64:33] + 1);
-                            end
-                            state <= DivEnd;
-                            cnt <= 0;
                         end
                     end else begin
                         state <= DivFree;
