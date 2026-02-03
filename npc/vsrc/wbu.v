39e58286773da369494202a47610180a0d27f0ce
tracer-ysyx <tracer@ysyx.org>
2026-02-03 17:30:31 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:31 up 2 days, 19:46,  1 user,  load average: 2.03, 1.74, 1.42

--- npc/vsrc/wbu.v
+++ npc/vsrc/wbu.v
@@ -230,17 +230,10 @@ module ysyx_25110270_wbu
         if(!rst_n) begin
             inst_r1         <= 0;
             inst_addr_r1    <= 0;
-            pc              <= 0;
             skip_r          <= 1'b0;
         end else begin
             inst_r1         <= I_inst;
             inst_addr_r1    <= I_inst_addr;
-            pc              <= O_flush ? O_flush_addr :
-                               (I_ls_addr == 0 ? 
-                               (I_ex_addr == 0 ? 
-                               (I_dec_addr == 0 ? I_if_addr : I_dec_addr) 
-                               : I_ex_addr) 
-                               : I_ls_addr);
             skip_r          <= I_device_skip;
         end
     end
@@ -249,7 +242,7 @@ module ysyx_25110270_wbu
         if(valid_r2) begin
             cpu_value
             (
-                skip_r, 1, inst_r1, inst_addr_r1, pc, 
+                skip_r, 1, inst_r1, inst_addr_r1, I_if_addr, 
                 gpr0, gpr1, gpr2, gpr3, gpr4, gpr5, gpr6, gpr7,
                 gpr8, gpr9, gpr10, gpr11, gpr12, gpr13, gpr14, gpr15,
                 gpr16, gpr17, gpr18, gpr19, gpr20, gpr21, gpr22, gpr23,
