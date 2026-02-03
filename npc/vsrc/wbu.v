72f6ca44e86577118332055427b758d0d5a6c8d4
tracer-ysyx <tracer@ysyx.org>
2026-02-03 15:35:23 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:35:23 up 2 days, 17:51,  1 user,  load average: 1.66, 1.26, 1.29

--- npc/vsrc/wbu.v
+++ npc/vsrc/wbu.v
@@ -182,7 +182,7 @@ module ysyx_25110270_wbu
 
 `ifdef DEBUG
     always @(posedge clk) begin
-        if(I_inst == 0 && I_inst_addr != 0) begin
+        if(valid & I_inst == 0 && I_inst_addr != 0) begin
             $error("Error: inst is 0 at addr %h!", I_inst_addr);
         end
     end
