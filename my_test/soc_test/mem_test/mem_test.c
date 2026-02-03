52972643a0f1337c564e273b00566867795ce5e9
tracer-ysyx <tracer@ysyx.org>
2026-02-03 09:44:13 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:44:13 up 2 days, 12:00,  1 user,  load average: 0.84, 1.05, 1.08

--- my_test/soc_test/mem_test/mem_test.c
+++ my_test/soc_test/mem_test/mem_test.c
@@ -22,21 +22,9 @@
             PUT(i, p_period, "w", bit); \
         } \
         printf("\n"); \
-        p = (volatile type *)(BASE + B2); \
-        for (uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + B2); i++) { \
-            p[i] = i + 12574237; \
-            PUT(i, p_period, "w", bit); \
-        } \
-        printf("\n"); \
         p = (volatile type *)(BASE + 2*B2); \
         for (uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + 2*B2); i++) { \
             p[i] = i + 12392227; \
-            PUT(i, p_period, "w", bit); \
-        } \
-        printf("\n"); \
-        p = (volatile type *)(BASE + 3*B2); \
-        for (uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + 3*B2); i++) { \
-            p[i] = i + 121358037; \
             PUT(i, p_period, "w", bit); \
         } \
         printf("\n"); \
@@ -49,27 +37,9 @@
             PUT(i, p_period, "r", bit); \
         } \
         printf("\n"); \
-        p = (volatile type *)(BASE + B2); \
-        for(uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + B2); i++) { \
-            if(p[i] != i + 12574237) { \
-                putstr("error\n"); \
-                return 1; \
-            } \
-            PUT(i, p_period, "r", bit); \
-        } \
-        printf("\n"); \
         p = (volatile type *)(BASE + 2*B2); \
         for(uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + 2*B2); i++) { \
             if(p[i] != i + 12392227) { \
-                putstr("error\n"); \
-                return 1; \
-            } \
-            PUT(i, p_period, "r", bit); \
-        } \
-        printf("\n"); \
-        p = (volatile type *)(BASE + 3*B2); \
-        for(uint32_t i = 0; &p[i] < (volatile type *)(BASE + SIZE + 3*B2); i++) { \
-            if(p[i] != i + 121358037) { \
                 putstr("error\n"); \
                 return 1; \
             } \
