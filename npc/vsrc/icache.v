505ce64428aaccd6c40fc67b00c903ca3d01d172
tracer-ysyx <tracer@ysyx.org>
2026-02-04 16:58:53 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:53 up 18:33,  1 user,  load average: 1.35, 1.74, 1.93

---
+++ npc/vsrc/icache.v
@@ -0,0 +1,13 @@
+
+//------------------------------------------------------------------------
+// icache
+//------------------------------------------------------------------------
+
+// module ysyx_25110270_icache
+// #(
+//     parameter TAG_WIDTH   = 20,
+//     parameter SET_NUM     = 8,
+//     parameter N_WAYS      = 4,
+//     parameter BLOCK_NUM   = 4,
+//     parameter BLOCK_SIZE  = 2
+// )
