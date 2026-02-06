455953e120009ac737d289c71516c936266f4ea2
tracer-ysyx <tracer@ysyx.org>
2026-02-06 14:12:50 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:12:50 up 21:16,  1 user,  load average: 0.12, 0.38, 0.62

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -39,7 +39,7 @@ run: run-env
 
 cache: run-env
 	$(call git_commit, "run NEMU for cache")
-	$(BINARY) $(ARGS) -b IMG=$(AM_HOME)/../am-kernels/tests/cpu-tests/build/crc32-riscv32e-ysyxsoc.bin
+	$(BINARY) $(ARGS) -b IMG=/home/hhh/Public/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/crc32-riscv32e-ysyxsoc.bin
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
