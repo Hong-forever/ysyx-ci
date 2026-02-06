1457954d28db487ae36b99b0394dfe91d495a724
tracer-ysyx <tracer@ysyx.org>
2026-02-06 14:14:58 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:14:58 up 21:19,  1 user,  load average: 1.36, 0.68, 0.69

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -34,9 +34,9 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
       if (is_write) serial_putc(serial_base[0]);
-      // else panic("do not support read");
+      else panic("do not support read");
       break;
-    default: panic("do not support offset = %d", offset);
+    // default: panic("do not support offset = %d", offset);
   }
 }
 
