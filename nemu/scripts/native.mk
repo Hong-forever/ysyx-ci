4c1c90b53af3384ed905430c6403b0692b68b5c6
tracer-ysyx <tracer@ysyx.org>
2026-02-06 10:46:49 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:46:49 up 17:33,  1 user,  load average: 1.76, 1.39, 1.14

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -28,7 +28,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
-IMG ?= $(AM_HOME)/am-kernels/tests/cpu-tests/build/crc32-riscv32e-ysyxsoc.bin
+IMG ?= $(AM_HOME)/../am-kernels/tests/cpu-tests/build/crc32-riscv32e-ysyxsoc.bin
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
