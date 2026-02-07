44e041177bc139c76d0795f17afe2357eeb92eaa
hhh <3224318821@qq.com>
2026-02-07 23:02:48 +0800

    test 1 for evaluate.

--- npc/vsrc/clint.v
+++ npc/vsrc/clint.v
@@ -16,20 +16,32 @@ module ysyx_25110270_clint
     input   wire                        awvalid_i,
     output  wire                        awready_o,
     input   wire    [ADDR_WIDTH-1:0 ]   awaddr_i,
+    input   wire    [3:0 ]              awid_i,
+    input   wire    [7:0 ]              awlen_i,
+    input   wire    [2:0 ]              awsize_i,
+    input   wire    [1:0 ]              awburst_i,
     input   wire                        wvalid_i,
     output  wire                        wready_o,
     input   wire    [DATA_WIDTH-1:0 ]   wdata_i,
     input   wire    [DATA_WIDTH/8-1:0]  wstrb_i,
+    input   wire                        wlast_i,
     output  wire                        bvalid_o,
     input   wire                        bready_i,
     output  wire    [1:0 ]              bresp_o,
+    output  wire    [3:0 ]              bid_o,
     input   wire                        arvalid_i,
     output  wire                        arready_o,
     input   wire    [ADDR_WIDTH-1:0 ]   araddr_i,
+    input   wire    [3:0 ]              arid_i,
+    input   wire    [7:0 ]              arlen_i,
+    input   wire    [2:0 ]              arsize_i,
+    input   wire    [1:0 ]              arburst_i,
     output  wire                        rvalid_o,
     input   wire                        rready_i,
     output  wire    [DATA_WIDTH-1:0 ]   rdata_o,
-    output  wire    [1:0 ]              rresp_o
+    output  wire    [1:0 ]              rresp_o,
+    output  wire                        rlast_o,
+    output  wire    [3:0 ]              rid_o
 );
 
     reg [2*DATA_WIDTH-1:0] mtime;
@@ -107,10 +119,13 @@ module ysyx_25110270_clint
     assign wready_o  = wready;
     assign bvalid_o  = wdata_valid;
     assign bresp_o   = 2'b00;
+    assign bid_o     = 4'b0000;
     assign arready_o = arready;
     assign rvalid_o  = rdata_valid;
     assign rdata_o   = rdata;
     assign rresp_o   = 2'b00;
+    assign rlast_o   = 1'b1;
+    assign rid_o     = 4'b0000;
 
 
 endmodule
