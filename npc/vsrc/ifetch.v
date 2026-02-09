82591f1fced5a522ba79fd4d64438274971f1758
tracer-ysyx <tracer@ysyx.org>
2026-02-09 23:47:10 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:47:10 up 4 days,  9:07,  1 user,  load average: 1.17, 0.71, 0.69

--- npc/vsrc/ifetch.v
+++ npc/vsrc/ifetch.v
@@ -156,9 +156,9 @@ module ysyx_25110270_ifetch
     #(
         .ADDR_WIDTH             (32                         ),
         .DATA_WIDTH             (32                         ),
-        .SET_NUM                (16                          ),
+        .SET_NUM                (32                          ),
         .N_WAYS                 (1                          ),
-        .BLOCK_SIZE             (32                          )
+        .BLOCK_SIZE             (8                          )
     ) icache
     (
         .clk                    (clk                        ),
