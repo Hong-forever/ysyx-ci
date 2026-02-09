98cd4a5cb7f50a1db0754be7ed090ff28fd7ed12
tracer-ysyx <tracer@ysyx.org>
2026-02-09 19:35:37 +0800

    > sim RTL for perf ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:35:37 up 4 days,  4:48,  1 user,  load average: 0.17, 0.49, 0.61

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -9,7 +9,7 @@ module ysyx_25110270_icache
     parameter ADDR_WIDTH  = 32,
     parameter SET_NUM     = 16,     // direct mapped cache sets
     parameter N_WAYS      = 1,      // number of ways, 1, 2, 4, 8
-    parameter BLOCK_SIZE  = 4       // in bytes
+    parameter BLOCK_SIZE  = 8       // in bytes
 )
 (
     input                           clk,
@@ -17,6 +17,7 @@ module ysyx_25110270_icache
     input       [ADDR_WIDTH-1:0]    I_addr,
     input                           I_wr,
     input       [DATA_WIDTH-1:0]    I_wdata,
+    input                           I_wlast,
     input                           I_valid,
     output      [DATA_WIDTH-1:0]    O_data,
     output                          O_valid,
@@ -35,18 +36,20 @@ module ysyx_25110270_icache
 
     // 存储器定义
     reg [TAG_WIDTH-1:0] tag_mem [0:SET_NUM*N_WAYS-1];
-    reg [DATA_WIDTH-1:0] data_mem [0:SET_NUM*N_WAYS*WORDS_PER_BLOCK-1];
+    reg [DATA_WIDTH-1:0] data_mem [0:SET_NUM*N_WAYS-1][0:WORDS_PER_BLOCK-1];
     reg valid_mem [0:SET_NUM*N_WAYS-1];
 
     wire [TAG_WIDTH-1:0]               tag;      // 标签位
     wire [SET_WIDTH-1:0]               index;    // 组索引
-    // wire [BLOCK_WIDTH-1:0]             offset;   // 组内偏移（字）
-    
+    wire [BLOCK_WIDTH-1:0]             offset;   // 块内字偏移
+
     assign tag     = I_addr[ADDR_WIDTH-1 : SET_WIDTH + BLOCK_WIDTH + 2];
     assign index   = I_addr[SET_WIDTH + BLOCK_WIDTH + 1 : BLOCK_WIDTH + 2];
-    // assign offset  = I_addr[BLOCK_WIDTH+1 : 2];
+    assign offset  = I_addr[BLOCK_WIDTH + 1 : 2];
+
 
     reg [1:0] state, nstate;
+    reg [BLOCK_WIDTH-1:0] cnt;
 
     reg hit;
     reg miss;
@@ -69,11 +72,7 @@ module ysyx_25110270_icache
                 IDLE: begin
                     hit = 1'b0;
                     miss = 1'b0;
-                    if(I_valid) begin
-                        nstate = READ;
-                    end else begin
-                        nstate = IDLE;
-                    end
+                    nstate = I_valid ? READ : IDLE;
                 end
                 READ: begin
                     if((tag_mem[index] == tag) & valid_mem[index]) begin
@@ -89,11 +88,7 @@ module ysyx_25110270_icache
                 MISS: begin
                     hit = 1'b0;
                     miss = 1'b1;
-                    if(I_valid & I_wr) begin
-                        nstate = IDLE;
-                    end else begin
-                        nstate = MISS;
-                    end
+                    nstate = I_valid & I_wr & I_wlast ? IDLE : MISS;
                 end
                 default: begin
                     hit = 1'b0;
@@ -104,22 +99,32 @@ module ysyx_25110270_icache
         end
     end
 
-    integer i;
+    integer i, j;
     always @(posedge clk) begin
         if(!rst_n) begin
             for(i = 0; i < SET_NUM*N_WAYS; i = i + 1) begin
-                data_mem[i] <= 0;
                 valid_mem[i] <= 0;
                 tag_mem[i] <= 0;
+                for(j=0; j < WORDS_PER_BLOCK; j = j + 1) begin
+                    data_mem[i][j] <= 0;
+                end
             end
+            cnt <= 0;
         end else if(I_valid && I_wr) begin
-            data_mem[index] <= I_wdata; // 简化为写入同一数据
+            data_mem[index][cnt] <= I_wdata;
             tag_mem[index] <= tag;
             valid_mem[index] <= 1'b1;
+            cnt <= I_wlast ? 0 : cnt + 1;
         end
     end
 
-    assign O_data = data_mem[index];
+    generate
+        if(BLOCK_WIDTH > 0) begin
+            assign O_data = data_mem[index][offset];
+        end else begin
+            assign O_data = data_mem[index][0];
+        end
+    endgenerate
     assign O_valid = hit;
     assign O_miss = miss;
 
