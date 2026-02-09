e78129f0f8cf6373f88cb782db72b2272ac1ae7b
tracer-ysyx <tracer@ysyx.org>
2026-02-09 10:12:57 +0800

    > sim RTL for perf ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:12:57 up 3 days, 19:18,  1 user,  load average: 1.60, 1.05, 0.71

--- npc/csrc/cpu/perf_cal.c
+++ npc/csrc/cpu/perf_cal.c
@@ -145,7 +145,7 @@ void perf_cal() {
 
     printf("\n===== IAMAT =====\n");
     printf("IAMAT        : %.2f\n", iamat);
-    printf("IHIT         : %.2ld(%.2f%%)\n", icache_hit, hit_per * 100);
+    printf("IHIT         : %.2f%%\n", hit_per * 100);
 
     printf("\n===== DAMAT =====\n");
     printf("L/S Delay    : %.2f\n", ls_inst_log.inst_nr  ? (double)ls_delay_total / (double)ls_inst_log.inst_nr : 0);
