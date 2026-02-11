78b77822e3f0062c403f61da3225f06b46dd8111
tracer-ysyx <tracer@ysyx.org>
2026-02-11 19:20:57 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:20:57 up 6 days,  5:12,  1 user,  load average: 2.22, 1.87, 1.56

--- abstract-machine/am/src/native/ioe.c
+++ abstract-machine/am/src/native/ioe.c
@@ -57,7 +57,6 @@ bool ioe_init() {
   panic_on(cpu_current() != 0, "call ioe_init() in other CPUs");
   panic_on(ioe_init_done, "double-initialization");
   __am_has_ioe = true;
-  printf("IOE initialized\n");
   return true;
 }
 
