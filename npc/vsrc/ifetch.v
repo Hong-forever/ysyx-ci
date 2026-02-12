0ebc2be9fd805c163c8a5311ecc31a49e259b7b8
tracer-ysyx <tracer@ysyx.org>
2026-02-12 16:07:42 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:07:42 up 7 days,  2:47,  1 user,  load average: 1.68, 0.95, 0.53

--- npc/vsrc/ifetch.v
+++ npc/vsrc/ifetch.v
@@ -117,7 +117,7 @@ module ysyx_25110270_ifetch
     
     always @(*) begin
         case(state)
-            IDLE:    nstate = CACHE;
+            IDLE:    nstate = req_ready ? CACHE : IDLE;
             CACHE:   nstate = cache_valid ? EXE : CACHE;
             EXE:     nstate = I_valid ? IDLE : EXE;
             default: nstate = IDLE;
