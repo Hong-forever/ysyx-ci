4d0171f91614f7f159a1e53175810e649efa88ef
tracer-ysyx <tracer@ysyx.org>
2026-02-12 17:03:35 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:03:35 up 7 days,  3:46,  1 user,  load average: 1.61, 1.15, 0.71

--- npc/vsrc/ifetch.v
+++ npc/vsrc/ifetch.v
@@ -77,7 +77,7 @@ module ysyx_25110270_ifetch
     wire [31:0] cache_data;
     wire [31:0] npc, pc_plus4;
 
-    wire req_valid_next = ~state[1] | I_valid & ~cache_valid;
+    wire req_valid_next = (~state[1] | I_valid) & ~cache_valid;
 
     always @(posedge clk) begin
         if(!rst_n) begin
