9381d1338b9605fc753d572c2f53499892c86420
tracer-ysyx <tracer@ysyx.org>
2026-02-12 18:22:23 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:22:23 up 7 days,  5:09,  1 user,  load average: 1.48, 0.54, 0.30

--- npc/vsrc/exe_mod.v
+++ npc/vsrc/exe_mod.v
@@ -65,7 +65,7 @@ module ysyx_25110270_alu
     (
         .I_shift_src            (I_alu_srca                                 ),
         .I_shift_amt            (I_alu_srcb[4:0]                            ),
-        .I_shift_left           (I_alu_ctrl == `ysyx_25110270_RV32I_F3_SLL  ),  // sll, slli指令左移
+        .I_shift_right          (I_alu_ctrl[2]                              ),  // srl, srli指令右移
         .I_shift_arith          (I_f7b5_en                                  ),
 
         .O_shift_result         (rv32i_shift_res                            )
@@ -119,33 +119,59 @@ module ysyx_25110270_barrel_shift
 )(
     input   wire    [WIDTH-1:0      ]   I_shift_src,
     input   wire    [4:0            ]   I_shift_amt,
-    input   wire                        I_shift_left,      // 1: left shift   0: right shift
+    input   wire                        I_shift_right,     // 1: right shift   0: left shift
     input   wire                        I_shift_arith,     // 1: arithmetic    0: logical
 
     output  wire    [WIDTH-1:0      ]   O_shift_result
 );
 
-    wire [WIDTH-1:0] lstage0, lstage1, lstage2, lstage3, lstage4;
-    wire [WIDTH-1:0] rstage0, rstage1, rstage2, rstage3, rstage4;
+    wire [WIDTH-1:0] stage0, stage1, stage2, stage3, stage4;
+    wire [WIDTH-1:0] data_reversed;
 
-    assign lstage0 = I_shift_amt[0] ? {I_shift_src[WIDTH-2:0], 1'b0} : I_shift_src;
-    assign lstage1 = I_shift_amt[1] ? {lstage0[WIDTH-3:0], 2'b0}     : lstage0;
-    assign lstage2 = I_shift_amt[2] ? {lstage1[WIDTH-5:0], 4'b0}     : lstage1;
-    assign lstage3 = I_shift_amt[3] ? {lstage2[WIDTH-9:0], 8'b0}     : lstage2;
-    assign lstage4 = I_shift_amt[4] ? {lstage3[WIDTH-17:0], 16'b0}   : lstage3;
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin: reverse_gen
+            assign data_reversed[i] = I_shift_src[WIDTH-1-i];
+        end
+    endgenerate
 
-    
-    // 选择输入数据
+    wire [WIDTH-1:0] shift_src = I_shift_right ? I_shift_src : data_reversed;
     wire fill_bit = I_shift_arith ? I_shift_src[WIDTH-1] : 1'b0;
+
+    assign stage0 = I_shift_amt[0] ? {fill_bit, shift_src[WIDTH-1:1]}     : shift_src;
+    assign stage1 = I_shift_amt[1] ? {{2{fill_bit}}, stage0[WIDTH-1:2]}   : stage0;
+    assign stage2 = I_shift_amt[2] ? {{4{fill_bit}}, stage1[WIDTH-1:4]}   : stage1;
+    assign stage3 = I_shift_amt[3] ? {{8{fill_bit}}, stage2[WIDTH-1:8]}   : stage2;
+    assign stage4 = I_shift_amt[4] ? {{16{fill_bit}}, stage3[WIDTH-1:16]} : stage3;
+
+    wire [WIDTH-1:0] shift_result = stage4;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin: output_gen
+            assign O_shift_result[i] = I_shift_right ? shift_result[i] : shift_result[WIDTH-1-i];
+        end
+    endgenerate
+
+    // wire [WIDTH-1:0] lstage0, lstage1, lstage2, lstage3, lstage4;
+    // wire [WIDTH-1:0] rstage0, rstage1, rstage2, rstage3, rstage4;
+
+    // assign lstage0 = I_shift_amt[0] ? {I_shift_src[WIDTH-2:0], 1'b0} : I_shift_src;
+    // assign lstage1 = I_shift_amt[1] ? {lstage0[WIDTH-3:0], 2'b0}     : lstage0;
+    // assign lstage2 = I_shift_amt[2] ? {lstage1[WIDTH-5:0], 4'b0}     : lstage1;
+    // assign lstage3 = I_shift_amt[3] ? {lstage2[WIDTH-9:0], 8'b0}     : lstage2;
+    // assign lstage4 = I_shift_amt[4] ? {lstage3[WIDTH-17:0], 16'b0}   : lstage3;
+
     
-    assign rstage0 = I_shift_amt[0] ? {fill_bit, I_shift_src[WIDTH-1:1]}    : I_shift_src;
-    assign rstage1 = I_shift_amt[1] ? {{2{fill_bit}}, rstage0[WIDTH-1:2]}   : rstage0;
-    assign rstage2 = I_shift_amt[2] ? {{4{fill_bit}}, rstage1[WIDTH-1:4]}   : rstage1;
-    assign rstage3 = I_shift_amt[3] ? {{8{fill_bit}}, rstage2[WIDTH-1:8]}   : rstage2;
-    assign rstage4 = I_shift_amt[4] ? {{16{fill_bit}}, rstage3[WIDTH-1:16]} : rstage3;
+    // // 选择输入数据
+    // wire fill_bit = I_shift_arith ? I_shift_src[WIDTH-1] : 1'b0;
+    
+    // assign rstage0 = I_shift_amt[0] ? {fill_bit, I_shift_src[WIDTH-1:1]}    : I_shift_src;
+    // assign rstage1 = I_shift_amt[1] ? {{2{fill_bit}}, rstage0[WIDTH-1:2]}   : rstage0;
+    // assign rstage2 = I_shift_amt[2] ? {{4{fill_bit}}, rstage1[WIDTH-1:4]}   : rstage1;
+    // assign rstage3 = I_shift_amt[3] ? {{8{fill_bit}}, rstage2[WIDTH-1:8]}   : rstage2;
+    // assign rstage4 = I_shift_amt[4] ? {{16{fill_bit}}, rstage3[WIDTH-1:16]} : rstage3;
     
     // 输出处理
-    assign O_shift_result  = I_shift_left ? lstage4 : rstage4;
+    // assign O_shift_result  = I_shift_right ? rstage4 : lstage4;
 
 endmodule
 
