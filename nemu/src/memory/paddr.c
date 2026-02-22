fcd842d6c22ff9feb2dfd11331c034379d337f95
tracer-ysyx <tracer@ysyx.org>
2026-02-22 15:37:38 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:37:38 up  6:07,  1 user,  load average: 0.52, 0.15, 0.05

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -82,7 +82,7 @@ static void mtrace(paddr_t addr, word_t data, int op) {
     static FILE *mtrace_fp = NULL;
         
     if (mtrace_fp == NULL) {
-        mtrace_fp = fopen("/tmp/dcachesim.bin", "wb");
+        mtrace_fp = fopen("/tmp/dcachesim.bin", "wh");
         if (mtrace_fp == NULL) {
             perror("Failed to open mtrace file");
         }
