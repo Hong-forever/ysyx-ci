e3c18a6cf3ea58caf10613e1c345e96d50a96736
tracer-ysyx <tracer@ysyx.org>
2026-02-22 16:09:43 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:09:43 up  6:39,  1 user,  load average: 0.58, 0.31, 0.19

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -92,6 +92,8 @@ static void mtrace(paddr_t addr, word_t data, int op) {
         }
     }
 
+    if(op != 0) printf("op is %x\n", op);
+
     uint64_t entry = ((uint64_t)op << 32) | (addr & 0xffffffff);
     
     if (mtrace_fp != NULL) {
