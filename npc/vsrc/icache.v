4569374f52a0cd540fba7dcf81e0023b3301bcdf
tracer-ysyx <tracer@ysyx.org>
2026-02-23 17:04:58 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:04:58 up 1 day,  8:15,  1 user,  load average: 2.37, 1.60, 1.39

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -50,9 +50,9 @@ module ysyx_25110270_icache
     reg [DATA_WIDTH-1:0] data_mem [0:SET_NUM*N_WAYS-1][0:WORDS_PER_BLOCK-1];
     reg valid_mem [0:SET_NUM*N_WAYS-1];
 
-    wire [TAG_WIDTH-1:0]               tag, miss_tag;      // 标签位
-    wire [SET_WIDTH-1:0]               index, miss_index;    // 组索引
-    wire [BLOCK_WIDTH-1:0]             offset, miss_offset;   // 块内字偏移
+    wire [TAG_WIDTH-1:0]               tag;      // 标签位
+    wire [SET_WIDTH-1:0]               index;    // 组索引
+    wire [BLOCK_WIDTH-1:0]             offset;   // 块内字偏移
 
     assign tag     = I_addr[ADDR_WIDTH-1 : SET_WIDTH + BLOCK_WIDTH + 2];
     assign index   = I_addr[SET_WIDTH + BLOCK_WIDTH + 1 : BLOCK_WIDTH + 2];
@@ -61,13 +61,6 @@ module ysyx_25110270_icache
 
     reg [2:0] state;
     reg [BLOCK_WIDTH-1:0] cnt;
-
-    reg [ADDR_WIDTH-1:0]  miss_addr;
-    assign miss_tag  = miss_addr[ADDR_WIDTH-1 : SET_WIDTH + BLOCK_WIDTH + 2];
-    assign miss_index = miss_addr[SET_WIDTH + BLOCK_WIDTH + 1 : BLOCK_WIDTH + 2];
-    assign miss_offset = miss_addr[BLOCK_WIDTH + 1 : 2];
-
-    wire [BLOCK_WIDTH-1:0] refill_offset = miss_offset + cnt;
 
     reg way_hit;
     generate
@@ -115,7 +108,6 @@ module ysyx_25110270_icache
                     data_mem[i][j] <= 0;
                 end
             end
-            miss_addr <= 0;
             cnt <= 0;
         end else if(I_clear && !clear_r) begin
             for(i = 0; i < SET_NUM*N_WAYS; i = i + 1) begin
@@ -123,19 +115,15 @@ module ysyx_25110270_icache
             end
         end else begin
             if(state[0] & ~hit) begin
-                miss_addr <= I_addr;
                 cnt <= 0;
             end else if(state[2] && I_rvalid) begin
-                data_mem[miss_index][refill_offset] <= I_rdata; // WRAP offset
+                data_mem[index][cnt] <= I_rdata;
                 cnt <= I_rlast ? 0 : cnt + 1;
-                tag_mem[miss_index] <= miss_tag;
-                valid_mem[miss_index] <= 1'b1;
+                tag_mem[index] <= tag;
+                valid_mem[index] <= I_rlast;
             end
         end
     end
-
-    wire refill_hit = state[2] && I_rvalid && (I_addr[ADDR_WIDTH-1:2] == {miss_tag, miss_index, refill_offset});
-    wire [DATA_WIDTH-1:0] refill_data = I_rdata;
 
     reg [DATA_WIDTH-1:0] odata_r;
     reg ovalid_r;
@@ -144,9 +132,6 @@ module ysyx_25110270_icache
         if(!rst_n) begin
             odata_r  = 0;
             ovalid_r = 1'b0;
-        end else if(refill_hit) begin
-            odata_r  = refill_data;
-            ovalid_r = 1'b1;
         end else if(state[0] & hit) begin
             odata_r = data_mem[index][offset];
             ovalid_r = 1'b1;
@@ -160,10 +145,11 @@ module ysyx_25110270_icache
     assign O_valid = ovalid_r;
 
     assign O_arvalid = state[1]; // REQ state
-    assign O_araddr  = miss_addr;
+    assign O_araddr  = {I_addr[ADDR_WIDTH-1:3], 3'b000};
     assign O_arlen   = WORDS_PER_BLOCK[7:0] - 8'b1;
     assign O_arsize  = 3'b010; // 4 bytes
-    assign O_arburst = 2'b10; // WRAP
+    assign O_arburst = 2'b01; // INCR
+    // assign O_arburst = 2'b10; // WRAP
     assign O_rready  = 1'b1;
 
 endmodule
