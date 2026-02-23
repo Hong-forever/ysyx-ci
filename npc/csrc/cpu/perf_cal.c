0b9b1c158d6faf98ccda9347e37d095812d2382c
tracer-ysyx <tracer@ysyx.org>
2026-02-23 14:38:54 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:38:54 up 1 day,  5:43,  1 user,  load average: 0.76, 0.56, 0.27

--- npc/csrc/cpu/perf_cal.c
+++ npc/csrc/cpu/perf_cal.c
@@ -91,7 +91,7 @@ extern "C" void ls_delay_cal(int begin_flag, int end_flag) {
 extern "C" void wb_inst_cycle_cal(uint32_t pc, uint32_t inst) {
     static uint8_t flag = 0;
     if(pc != 0 && inst != 0 ) {
-        printf("WB Cal: PC=0x%08x, inst=0x%08x\n", pc, inst);
+        printf("WB Cal: PC=0x%08x, inst=0x%08x, NO: %lu\n", pc, inst, wb_inst);
         flag = 1;
         act_wb_inst++;
         if(pc != inst_buffer[wb_inst % 5].pc) {
