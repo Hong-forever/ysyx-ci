0a82e2252eacae848684d713d38ef5284311282d
tracer-ysyx <tracer@ysyx.org>
2026-02-24 22:06:50 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:06:50 up  3:43,  1 user,  load average: 1.40, 0.49, 0.27

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -120,7 +120,7 @@ module ysyx_25110270_icache
     always @(posedge clk) begin
         if(rst) begin
             ovalid_r <= 1'b0;
-        end else if(I_valid & hit) begin
+        end else if(I_valid & hit & !ovalid_r) begin
             odata_r <= data_mem[index][offset];
             ovalid_r <= 1'b1;
         end else begin
