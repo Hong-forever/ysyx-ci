21d2242cd18b3a640112f66d162c2f4dbc4ca27a
tracer-ysyx <tracer@ysyx.org>
2026-02-25 22:57:59 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:57:59 up 1 day,  5:44,  1 user,  load average: 0.77, 0.52, 0.41

--- my_test/soc_test/branchsim/branchsim.c
+++ my_test/soc_test/branchsim/branchsim.c
@@ -61,21 +61,21 @@ bool branchsim_access(uint32_t inst, uint32_t pc, uint32_t target, bool is_taken
             btb[(pc>>2) % BTB_SIZE] = target;
         }
 
-        // bool is_taken_final = !is_correct;
-        // static FILE *log_fp = NULL;
-        // if (!log_fp) {
-        //     log_fp = fopen("/tmp/branchsim_log.bin", "wb");
-        //     if (!log_fp) {
-        //         perror("Failed to open log file");
-        //     }
-        // }
+        bool is_taken_final = !is_correct;
+        static FILE *log_fp = NULL;
+        if (!log_fp) {
+            log_fp = fopen("/tmp/branchsim_log.bin", "wb");
+            if (!log_fp) {
+                perror("Failed to open log file");
+            }
+        }
 
-        // if (log_fp) {
-        //     uint64_t log_entry = ((uint64_t)inst) | ((uint64_t)pc << 32);
-        //     uint64_t log_entry2 =  ((uint64_t)is_taken | (uint64_t)is_taken_final << 1);
-        //     fwrite(&log_entry, sizeof(uint64_t), 1, log_fp);
-        //     fwrite(&log_entry2, sizeof(uint64_t), 1, log_fp);
-        // }
+        if (log_fp) {
+            uint64_t log_entry = ((uint64_t)inst) | ((uint64_t)pc << 32);
+            uint64_t log_entry2 =  ((uint64_t)is_taken | (uint64_t)is_taken_final << 1);
+            fwrite(&log_entry, sizeof(uint64_t), 1, log_fp);
+            fwrite(&log_entry2, sizeof(uint64_t), 1, log_fp);
+        }
 
     } else if ((inst & 0x7f) == 0x67) { // JALR
         jalr_inst_nr++;
