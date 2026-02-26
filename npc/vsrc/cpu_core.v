739ec2b8989013edf30a24a7a5893a07c181ebaa
tracer-ysyx <tracer@ysyx.org>
2026-02-26 16:49:56 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:49:56 up 2 days, 32 min,  1 user,  load average: 1.51, 0.65, 0.31

--- npc/vsrc/cpu_core.v
+++ npc/vsrc/cpu_core.v
@@ -277,7 +277,6 @@ module ysyx_25110270_cpu_core
     wire                                dbus_rlast;
     wire [3:0 ]                         dbus_rid;
 
-    wire ifu_fence_i = O_ex_except[`ysyx_25110270_EXCPT_FENCE_I];
     ysyx_25110270_ifetch u_ifetch
     (
         .clk                    (clk                        ),
@@ -293,8 +292,6 @@ module ysyx_25110270_cpu_core
 
         .I_flush                (O_flush                    ),
         .I_flush_addr           (O_flush_addr               ),
-
-        .I_fence_i              (ifu_fence_i                ),
 
         .O_inst                 (O_if_inst                  ),
         .O_inst_addr            (O_if_inst_addr             ),
--- npc/vsrc/ifetch.v
+++ npc/vsrc/ifetch.v
@@ -20,8 +20,6 @@ module ysyx_25110270_ifetch
     input   wire                        I_flush,            // 指令冲刷
     input   wire    [31:0]              I_flush_addr,       // 冲刷跳转地址
 
-    input   wire                        I_fence_i,          // 指令同步
-
     output  wire    [31:0]              O_inst,
     output  wire    [31:0]              O_inst_addr,
 
@@ -70,6 +68,8 @@ module ysyx_25110270_ifetch
     wire [31:0] inst;
     wire [31:0] pc_plus4;
 
+    wire icache_clear = (inst == `ysyx_25110270_RV_FENCE_I) & O_valid;
+
     ysyx_25110270_icache 
     #(
         .ADDR_WIDTH             (32                         ),
@@ -88,7 +88,7 @@ module ysyx_25110270_ifetch
         .I_addr                 (pc                         ),
         .O_data                 (inst                       ),
 
-        .I_clear                (I_fence_i                  ),
+        .I_clear                (icache_clear               ),
 
         .O_arvalid              (ibus_arvalid               ),
         .I_arready              (ibus_arready               ),
@@ -105,20 +105,21 @@ module ysyx_25110270_ifetch
 
     wire pred_taken;
     wire [31:0] pred_target;
+    parameter SRC_W = 1;
     ysyx_25110270_branch_predictor
     #(
-        .SRC_WIDTH              (2                          )
+        .SRC_WIDTH              (SRC_W                      )
     ) branch_predictor
     (
         .clk                    (clk                        ),
         .rst                    (rst                        ),
 
         .update                 (I_btb_update               ),
-        .update_src             (I_bru_source[3:2]          ),
+        .update_src             (I_bru_source[SRC_W+1:2]    ),
         .update_dst             (I_bru_target               ),
 
         .inst                   (inst                       ),
-        .pc                     (pc[3:2]                    ),
+        .pc                     (pc[SRC_W+1:2]              ),
         .taken                  (pred_taken                 ),
         .target                 (pred_target                )
     );
