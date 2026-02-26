6f6339b8a6765903d69a89cf14699a24a8e628e2
tracer-ysyx <tracer@ysyx.org>
2026-02-26 10:31:10 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:31:10 up 1 day, 17:51,  1 user,  load average: 0.05, 0.05, 0.07

--- my_test/soc_test/branchsim/branchsim.c
+++ my_test/soc_test/branchsim/branchsim.c
@@ -44,7 +44,7 @@ bool branchsim_access(uint32_t inst, uint32_t pc, uint32_t target, bool is_taken
 
         bool is_correct = (pred_taken && is_taken && predicted_target == target) || (!pred_taken && !is_taken);
 
-        if(pc == 0xa0000264) {
+        if(pc == 0xa0000264 && is_taken == false && is_correct == false) {
             printf("Debug: PC=0x%08x, Inst=0x%08x, Target=0x%08x, Taken=%d, PredTaken=%d, PredTarget=0x%08x, Correct=%d\n", 
                     pc, inst, target, is_taken, pred_taken, predicted_target, is_correct);
         }
