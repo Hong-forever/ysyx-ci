57d115b88e0f162db9895d9526eb211a5675ddd8
tracer-ysyx <tracer@ysyx.org>
2026-02-26 17:16:00 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:16:00 up 2 days,  1:00,  1 user,  load average: 1.44, 0.70, 0.48

--- npc/vsrc/branch_predictor.v
+++ npc/vsrc/branch_predictor.v
@@ -21,10 +21,9 @@ module ysyx_25110270_branch_predictor
     output  wire    [31:0]              target        // 输出到IF阶段的分支目标地址
 );
 
-    wire valid;
     wire [31:0] branch_target;
 
-    assign taken = (((inst[6:0] == 7'h63) & inst[31]) | (inst[6:0] == 7'h6f)) & valid;
+    assign taken = (((inst[6:0] == 7'h63) & inst[31]) | (inst[6:0] == 7'h6f));
     assign target = branch_target;
 
     ysyx_25110270_branch_target_buffer 
@@ -38,7 +37,6 @@ module ysyx_25110270_branch_predictor
         .update_src             (update_src                 ),
         .update_dst             (update_dst                 ),
         .source                 (pc                         ),
-        .valid                  (valid                      ),
         .target                 (branch_target              )
     );
 
@@ -56,26 +54,18 @@ module ysyx_25110270_branch_target_buffer
     input   wire    [31:0]              update_dst,
 
     input   wire    [SRC_WIDTH-1:0]     source,
-    output  wire                        valid,
     output  wire    [31:0]              target
 );
 
     reg [31:0] btb [0:(1<<SRC_WIDTH)-1];
-    reg btb_valid [0:(1<<SRC_WIDTH)-1];
 
     integer i;
     always @(posedge clk) begin
-        if(rst) begin
-            for(i = 0; i < (1<<SRC_WIDTH); i = i + 1) begin
-                btb_valid[i] <= 0;
-            end
-        end else if(update) begin
+        if(update) begin
             btb[update_src] <= update_dst;
-            btb_valid[update_src] <= 1;
         end
     end
 
-    assign valid = btb_valid[source];
     assign target = btb[source];
 
 endmodule
