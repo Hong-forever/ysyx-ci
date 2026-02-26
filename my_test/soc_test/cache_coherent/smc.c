c2ab9311a80e7cca7beb6d97ed1198c46cac0027
tracer-ysyx <tracer@ysyx.org>
2026-02-26 12:47:47 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:47:47 up 1 day, 20:15,  1 user,  load average: 0.88, 0.81, 0.51

--- my_test/soc_test/cache_coherent/smc.c
+++ my_test/soc_test/cache_coherent/smc.c
@@ -11,7 +11,7 @@ int main() {
                "again:"
                "sb t1, (a1);"
                "sw t2, (a2);"
-               "fence.i;"
+              //  "fence.i;"
                "j again;"
               );
   return 0;
