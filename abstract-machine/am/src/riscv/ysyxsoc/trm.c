d5b4fe83a156997f46ffbd4c2c7b2289381b265f
tracer-ysyx <tracer@ysyx.org>
2026-02-26 19:49:27 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:49:27 up 2 days,  3:46,  1 user,  load average: 1.41, 0.86, 0.70

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -37,15 +37,15 @@ void halt(int code)
 void _trm_init()
 {
     serial_init();
-    // uint32_t ysyx_logo = ysyxsoc_csr_read(CSR_MVENDORID);
-    // // printf("mvendorid: 0x%08x\n", ysyx_logo);
-    // uint32_t stu_num = ysyxsoc_csr_read(CSR_MARCHID);
-    // printf("========================================\n");
-    // printf("Version: %s\n", VERSION);
-    // printf("Welcome to %s!\n", ysyx_logo == YSYX_LOGO ? "YSYX" : "Unknown");
-    // printf("Student Number: ysyx_%x\n", stu_num);
-    // printf("Name: %s\n", "HUANG HAIHONG");
-    // printf("========================================\n");
+    uint32_t ysyx_logo = ysyxsoc_csr_read(CSR_MVENDORID);
+    // printf("mvendorid: 0x%08x\n", ysyx_logo);
+    uint32_t stu_num = ysyxsoc_csr_read(CSR_MARCHID);
+    printf("========================================\n");
+    printf("Version: %s\n", VERSION);
+    printf("Welcome to %s!\n", ysyx_logo == YSYX_LOGO ? "YSYX" : "Unknown");
+    printf("Student Number: ysyx_%x\n", stu_num);
+    printf("Name: %s\n", "HUANG HAIHONG");
+    printf("========================================\n");
     
     int ret = main(mainargs);
     halt(ret);
