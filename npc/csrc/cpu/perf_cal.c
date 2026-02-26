62c69e22dc680881d7357a790217410193e3f16a
tracer-ysyx <tracer@ysyx.org>
2026-02-26 11:02:25 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:02:25 up 1 day, 18:24,  1 user,  load average: 0.84, 0.45, 0.36

--- npc/csrc/cpu/perf_cal.c
+++ npc/csrc/cpu/perf_cal.c
@@ -83,7 +83,7 @@ extern "C" void jump_br_cal(uint32_t inst, uint32_t pc, uint32_t target, bool is
 
             if (log_fp) {
                 uint64_t log_entry = ((uint64_t)inst) | ((uint64_t)pc << 32);
-                uint64_t log_entry2 = ((uint64_t)is_taken | ((uint64_t)is_taken_final << 8));
+                uint64_t log_entry2 = ((uint64_t)is_taken | ((uint64_t)is_taken_final << 1));
                 fwrite(&log_entry, sizeof(uint64_t), 1, log_fp);
                 fwrite(&log_entry2, sizeof(uint64_t), 1, log_fp);
             }    
