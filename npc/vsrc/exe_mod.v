b1a14d9ddaf7170a0adc24ec679da4a6858ae211
tracer-ysyx <tracer@ysyx.org>
2026-02-27 12:21:01 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:21:01 up 2 days, 20:50,  1 user,  load average: 1.35, 0.78, 0.56

--- npc/vsrc/exe_mod.v
+++ npc/vsrc/exe_mod.v
@@ -83,7 +83,6 @@ module ysyx_25110270_bru
     input   wire                                    I_src_eq,
     input   wire                                    I_src_lt,
     input   wire    [2:0]                           I_bru_ctrl,
-    input   wire                                    I_br_valid,
     
     output  wire                                    O_bru_taken
 );
@@ -102,7 +101,7 @@ module ysyx_25110270_bru
         endcase
     end
 
-    assign O_bru_taken = bru_taken & I_br_valid;
+    assign O_bru_taken = bru_taken;
 
 
 endmodule
