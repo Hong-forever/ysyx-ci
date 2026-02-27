b9325ea030c59f4ca51215132fe6bca2d9e00463
tracer-ysyx <tracer@ysyx.org>
2026-02-27 15:46:00 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:46:00 up 3 days, 20 min,  1 user,  load average: 1.19, 0.46, 0.23

--- npc/vsrc/exec.v
+++ npc/vsrc/exec.v
@@ -277,12 +277,14 @@ module ysyx_25110270_exec
         end
     end
 
-    reg valid;
+    reg valid, valid_r2;
     always @(posedge clk) begin
         if(rst) begin
             valid <= 1'b0;
+            valid_r2 <= 1'b0;
         end else begin
             valid <= I_valid;
+            valid_r2 <= valid;
         end
     end
 `endif
@@ -292,8 +294,8 @@ module ysyx_25110270_exec
     import "DPI-C" function void jump_br_cal(input int inst, input int pc, input int target, input int is_taken, input int is_taken_final, input int pred_target);
 
     always @(posedge clk) begin
-        if(I_br_valid & valid) begin
-            jump_br_cal(I_inst, I_inst_addr, agu_result, bru_taken, bru_taken_final, I_pred_target);
+        if(I_br_valid & valid_r2) begin
+            jump_br_cal(I_inst, I_inst_addr, agu_result_r, bru_taken_r, bru_taken_final, I_pred_target);
         end
     end
 
