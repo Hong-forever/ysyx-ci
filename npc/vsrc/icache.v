7c457fd91ae600f9a98817ddcf7467a3cabe5cec
tracer-ysyx <tracer@ysyx.org>
2026-02-27 14:41:35 +0800

    > sim RTL for perf ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:41:35 up 2 days, 23:15,  1 user,  load average: 0.53, 0.84, 0.79

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -101,29 +101,29 @@ module ysyx_25110270_icache
     reg [DATA_WIDTH-1:0] odata_r;
     reg ovalid_r;
 
-    // always @(*) begin
-    //     if(rst) begin
-    //         ovalid_r = 1'b0;
-    //         odata_r = 0;
-    //     end else if(I_valid && hit) begin
-    //         ovalid_r = 1'b1;
-    //         odata_r = data_mem[index][offset];
-    //     end else begin
-    //         ovalid_r = 1'b0;
-    //         odata_r = 0;
-    //     end
-    // end
-
-    always @(posedge clk) begin
+    always @(*) begin
         if(rst) begin
-            ovalid_r <= 1'b0;
-        end else if(I_valid & hit & !ovalid_r) begin
-            odata_r <= data_mem[index][offset];
-            ovalid_r <= 1'b1;
+            ovalid_r = 1'b0;
+            odata_r = 0;
+        end else if(I_valid && hit) begin
+            ovalid_r = 1'b1;
+            odata_r = data_mem[index][offset];
         end else begin
-            ovalid_r <= 1'b0;
+            ovalid_r = 1'b0;
+            odata_r = 0;
         end
     end
+
+    // always @(posedge clk) begin
+    //     if(rst) begin
+    //         ovalid_r <= 1'b0;
+    //     end else if(I_valid & hit & !ovalid_r) begin
+    //         odata_r <= data_mem[index][offset];
+    //         ovalid_r <= 1'b1;
+    //     end else begin
+    //         ovalid_r <= 1'b0;
+    //     end
+    // end
 
     assign O_data  = odata_r;
     assign O_valid = ovalid_r;
