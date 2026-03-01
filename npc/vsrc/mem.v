36a9fe09ff7fb766694c5f80e86abdd1c0aadb45
tracer-ysyx <tracer@ysyx.org>
2026-03-01 22:55:39 +0800

    > sim RTL with iverilog ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:55:39 up 5 days,  9:01,  1 user,  load average: 1.27, 1.15, 1.16

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -134,7 +134,7 @@ module mem
     assign rresp_o   = 2'b00;
 
     wire [31:0] mem = mem_array[32'h8000_0000];
-    wire [31:0] araddr = araddr_i[$clog2(MEM_DEPTH*4)+1:2];
+    wire [31:0] araddr = araddr_i[31:2];
     wire [31:0] araddr_ref = araddr_i[31:2];
 
     initial begin
