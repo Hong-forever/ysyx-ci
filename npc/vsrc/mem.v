847a51dee6525a9cdc6f22c858dcfab83b078b82
tracer-ysyx <tracer@ysyx.org>
2026-03-01 22:55:02 +0800

    > sim RTL with iverilog ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:55:02 up 5 days,  9:00,  1 user,  load average: 1.18, 1.12, 1.15

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -86,7 +86,7 @@ module mem
                 rvalid <= 1'b0;
                 rlast <= 1'b0;
             end else if(arvalid_i && arready || flag) begin
-                rdata <= mem_array[araddr_i[$clog2(MEM_DEPTH*4)+1:2] + cnt];
+                rdata <= mem_array[araddr_i[31:2] + cnt];
                 rvalid <= 1'b1;
                 if(cnt == arlen_i) begin
                     rlast <= 1'b1;
@@ -103,10 +103,10 @@ module mem
 
     wire [31:0] wdata_mask = 
     {
-        (wstrb_i[3] ? wdata_i[31:24] : mem_array[awaddr_i[$clog2(MEM_DEPTH*4)+1:2]][31:24]),
-        (wstrb_i[2] ? wdata_i[23:16] : mem_array[awaddr_i[$clog2(MEM_DEPTH*4)+1:2]][23:16]),
-        (wstrb_i[1] ? wdata_i[15:8 ] : mem_array[awaddr_i[$clog2(MEM_DEPTH*4)+1:2]][15:8 ]),
-        (wstrb_i[0] ? wdata_i[7 :0 ] : mem_array[awaddr_i[$clog2(MEM_DEPTH*4)+1:2]][7 :0 ])
+        (wstrb_i[3] ? wdata_i[31:24] : mem_array[awaddr_i[31:2]][31:24]),
+        (wstrb_i[2] ? wdata_i[23:16] : mem_array[awaddr_i[31:2]][23:16]),
+        (wstrb_i[1] ? wdata_i[15:8 ] : mem_array[awaddr_i[31:2]][15:8 ]),
+        (wstrb_i[0] ? wdata_i[7 :0 ] : mem_array[awaddr_i[31:2]][7 :0 ])
     };
 
     reg bvalid;
@@ -117,7 +117,7 @@ module mem
             if(bvalid_o && bready_i) begin
                 bvalid <= 1'b0;
             end else if(wvalid_i && wready) begin
-                mem_array[awaddr_i[$clog2(MEM_DEPTH*4)-1:2]] <= wdata_mask;
+                mem_array[awaddr_i[31:2]] <= wdata_mask;
                 bvalid <= 1'b1;
             end
         end
