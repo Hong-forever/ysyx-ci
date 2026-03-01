729dc441ca259d4969edfc3e076997580ccf7a78
tracer-ysyx <tracer@ysyx.org>
2026-03-01 20:21:08 +0800

    > sim RTL for perf ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:08 up 5 days,  6:21,  1 user,  load average: 0.00, 0.02, 0.00

---
+++ npc/vsrc/clint.v
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------
+// clint模块
+//------------------------------------------------------------------------
+
+module ysyx_25110270_clint
+(
+    input   wire                        clk,        //时钟输入
+    input   wire                        rst,        //复位输入
+
+    input   wire    [63:0]              time_i,     //mtime寄存器输入
+
+    // AXI接口
+    input   wire                        awvalid_i,
+    output  wire                        awready_o,
+    input   wire    [31:0]              awaddr_i,
+    input   wire    [3:0 ]              awid_i,
+    input   wire    [7:0 ]              awlen_i,
+    input   wire    [2:0 ]              awsize_i,
+    input   wire    [1:0 ]              awburst_i,
+    input   wire                        wvalid_i,
+    output  wire                        wready_o,
+    input   wire    [31:0]              wdata_i,
+    input   wire    [3:0]               wstrb_i,
+    input   wire                        wlast_i,
+    output  wire                        bvalid_o,
+    input   wire                        bready_i,
+    output  wire    [1:0 ]              bresp_o,
+    output  wire    [3:0 ]              bid_o,
+    input   wire                        arvalid_i,
+    output  wire                        arready_o,
+    input   wire    [31:0]              araddr_i,
+    input   wire    [3:0 ]              arid_i,
+    input   wire    [7:0 ]              arlen_i,
+    input   wire    [2:0 ]              arsize_i,
+    input   wire    [1:0 ]              arburst_i,
+    output  wire                        rvalid_o,
+    input   wire                        rready_i,
+    output  wire    [31:0]              rdata_o,
+    output  wire    [1:0 ]              rresp_o,
+    output  wire                        rlast_o,
+    output  wire    [3:0 ]              rid_o
+);
+
+    reg rdata_valid;
+    always @(posedge clk) begin
+        if(rst) begin
+            rdata_valid <= 1'b0;
+        end else begin
+            if(rvalid_o && rready_i) begin
+                rdata_valid <= 1'b0;
+            end else if(arvalid_i) begin
+                rdata_valid <= 1'b1;
+            end
+        end
+    end
+
+
+
+    assign awready_o = 1'b0;
+    assign wready_o  = 1'b0;
+    assign bvalid_o  = 1'b0;
+    assign bresp_o   = 2'b00;
+    assign bid_o     = 4'b0000;
+    assign arready_o = 1'b1;
+    assign rvalid_o  = rdata_valid;
+    assign rdata_o   = araddr_i[2] ? time_i[63:32] : time_i[31:0];
+    assign rresp_o   = 2'b00;
+    assign rlast_o   = 1'b1;
+    assign rid_o     = 4'b0000;
+
+
+endmodule
---
+++ npc/vsrc/decoder.v
@@ -0,0 +1,354 @@
+`include "defines.v"
+
+//------------------------------------------------------------------------
+// 译码单元
+//------------------------------------------------------------------------
+
+module ysyx_25110270_decoder 
+(
+    input   wire                                    clk,
+    input   wire                                    rst,
+
+    input   wire    [31:0                       ]   I_inst,
+    input   wire    [31:0                       ]   I_inst_addr,
+    
+    input   wire                                    I_ready,
+    output  wire                                    O_ready,
+    output  wire                                    O_valid,
+
+    output  wire    [`ysyx_25110270_RegAddrBus  ]   O_rs1_raddr,        //regfiles读通用寄存器1地址
+    output  wire    [`ysyx_25110270_RegAddrBus  ]   O_rs2_raddr,        //regfiles读通用寄存器2地址       
+    output  wire    [`ysyx_25110270_CsrMapBus   ]   O_csr_addr,         //CSR寄存器地址
+
+    output  wire    [31:0                       ]   O_inst,             //指令内容
+    output  wire    [31:0                       ]   O_inst_addr,        //指令地址
+
+    output  wire    [31:0                       ]   O_imm,              //立即数
+    output  wire                                    O_rd_we,            //写通用寄存器标志
+    output  wire    [`ysyx_25110270_RegAddrBus  ]   O_rd_waddr,         //写通用寄存器地址
+    output  wire    [2:0                        ]   O_op,
+
+    output  wire    [1:0                        ]   O_alu_srca_sel,
+    output  wire    [1:0                        ]   O_alu_srcb_sel,
+    output  wire    [1:0                        ]   O_agu_src_sel,
+    output  wire                                    O_csr_src_sel,
+    
+    output  wire                                    O_ld_valid,         //访存有效标志
+    output  wire                                    O_st_valid,         //访存有效标志
+    output  wire                                    O_br_valid,         //跳转有效标志
+    output  wire                                    O_csr_valid,        //CSR指令有效标志
+    output  wire                                    O_f7b5_en,          //funct7[5]使能，针对srai, sub, sra指令
+    output  wire                                    O_sign,             //有符号位
+    output  wire    [`ysyx_25110270_ExceptBus   ]   O_except,
+
+    //forward
+    output  wire                                    O_rs1_re,
+    output  wire                                    O_rs2_re
+);
+    
+    //------------------------------------------------------------------------
+    // 指令解码
+    //------------------------------------------------------------------------
+    wire [6:0]  opcode;
+    wire [2:0]  funct3;
+    wire [6:0]  funct7;
+    wire [3:0]  rd;
+    wire [3:0]  rs1;
+    wire [3:0]  rs2;
+
+    ysyx_25110270_RV32_Inst_Unpack inst_unpack
+    (
+        .I_inst                 (I_inst                     ),
+        .opcode                 (opcode                     ),
+        .funct3                 (funct3                     ),
+        .funct7                 (funct7                     ),
+        .rd                     (rd                         ),
+        .rs1                    (rs1                        ),
+        .rs2                    (rs2                        )
+    );
+
+    //------------------------------------------------------------------------
+    // 立即数生成
+    //------------------------------------------------------------------------
+    wire [31:0] rv32i_i_type_imm = {{20{I_inst[31]}}, I_inst[31:20]};
+    wire [31:0] rv32i_s_type_imm = {{20{I_inst[31]}}, I_inst[31:25], I_inst[11:7]};
+    wire [31:0] rv32i_u_type_imm = {I_inst[31:12], 12'b0};
+    wire [31:0] rv32i_b_type_imm = {{20{I_inst[31]}}, I_inst[7], I_inst[30:25], I_inst[11:8], 1'b0};
+    wire [31:0] rv32i_j_type_imm = {{12{I_inst[31]}}, I_inst[19:12], I_inst[20], I_inst[30:21], 1'b0};
+    wire [31:0] rv_csr_type_imm  = {27'h0, I_inst[19:15]};
+
+    reg [31:0] imm;
+    always @(*) begin
+        case(opcode)
+            `ysyx_25110270_RV32I_OP_TYPE_IL,
+            `ysyx_25110270_RV32I_OP_TYPE_I,
+            `ysyx_25110270_RV32I_OP_JALR:       imm = rv32i_i_type_imm;
+
+            `ysyx_25110270_RV32I_OP_TYPE_S:     imm = rv32i_s_type_imm;
+
+            `ysyx_25110270_RV32I_OP_AUIPC,
+            `ysyx_25110270_RV32I_OP_LUI:        imm = rv32i_u_type_imm;
+
+            `ysyx_25110270_RV32I_OP_TYPE_B:     imm = rv32i_b_type_imm;
+            `ysyx_25110270_RV32I_OP_JAL:        imm = rv32i_j_type_imm;
+            `ysyx_25110270_RV_OP_CSR:           imm = rv_csr_type_imm;
+            default:                            imm = 0;
+        endcase
+    end
+
+    //------------------------------------------------------------------------
+    // 控制信号生成
+    //------------------------------------------------------------------------
+
+    /*=======================================================================
+        [18:16]:    op
+        [15   ]:    csr_src_sel
+        [14:13]:    agu_src_sel  
+        [12:11]:    alu_srcb_sel
+        [10:9 ]:    alu_srca_sel
+        [8    ]:    f7b5_en    ---> srai, sub, sra
+        [7    ]:    csr_valid
+        [6    ]:    br_valid
+        [5    ]:    st_valid
+        [4    ]:    ld_valid
+        [3    ]:    sign
+        [2    ]:    rs2_re
+        [1    ]:    rs1_re
+        [0    ]:    rd_we
+    ========================================================================*/
+    
+    localparam bit_rd_we     = 0;
+    localparam bit_rs1_re    = 1;
+    localparam bit_rs2_re    = 2;
+    localparam bit_sign      = 3;
+    localparam bit_ld_valid  = 4;
+    localparam bit_st_valid  = 5;
+    localparam bit_br_valid  = 6;
+    localparam bit_csr_valid = 7;
+    localparam bit_f7b5_en   = 8;
+    localparam bit_alu_srca  = 9;
+    localparam bit_alu_srcb  = 11;
+    localparam bit_agu_src   = 13;
+    localparam bit_csr_src   = 15;
+    localparam bit_op        = 16;
+
+    reg [18:0] basic_ctrl;
+
+    always @(*) begin
+        basic_ctrl = 0;
+        case(opcode)
+            `ysyx_25110270_RV32I_OP_TYPE_IL: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_ld_valid     ] = 1'b1;
+                basic_ctrl[bit_agu_src +: 2 ] = `ysyx_25110270_AGUSRC_RS1;
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            `ysyx_25110270_RV32I_OP_TYPE_I: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_sign         ] = (funct3 != `ysyx_25110270_RV32I_F3_SLTIU);  // no sltiu
+                basic_ctrl[bit_f7b5_en      ] = I_inst[30] & (funct3 == `ysyx_25110270_RV32I_F3_SRI);  // only srai has funct7[5] = 1
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_RS1;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_IMM;
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            `ysyx_25110270_RV32I_OP_AUIPC: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_sign         ] = 1'b1;
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_PC;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_IMM;
+                basic_ctrl[bit_op +: 3      ] = `ysyx_25110270_RV32I_F3_ADD_SUB;
+            end
+            `ysyx_25110270_RV32I_OP_LUI: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_sign         ] = 1'b1;
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_0;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_IMM;
+                basic_ctrl[bit_op +: 3      ] = `ysyx_25110270_RV32I_F3_ADD_SUB;
+            end
+            `ysyx_25110270_RV32I_OP_TYPE_S: begin
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_rs2_re       ] = 1'b1;
+                basic_ctrl[bit_st_valid     ] = 1'b1;
+                basic_ctrl[bit_agu_src +: 2 ] = `ysyx_25110270_AGUSRC_RS1;
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            `ysyx_25110270_RV32IM_OP_TYPE_R: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_rs2_re       ] = 1'b1;
+                basic_ctrl[bit_sign         ] = (funct3 != `ysyx_25110270_RV32I_F3_SLTU);  // no sltu
+                basic_ctrl[bit_f7b5_en      ] = I_inst[30];
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_RS1;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_RS2;
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            `ysyx_25110270_RV32I_OP_TYPE_B: begin
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_rs2_re       ] = 1'b1;
+                basic_ctrl[bit_sign         ] = ~(funct3[2] & funct3[1]);   // no bltu, bgeu
+                basic_ctrl[bit_br_valid     ] = 1'b1;
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_RS1;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_RS2;
+                basic_ctrl[bit_agu_src +: 2 ] = `ysyx_25110270_AGUSRC_PC;
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            `ysyx_25110270_RV32I_OP_JALR: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_sign         ] = 1'b1;
+                basic_ctrl[bit_br_valid     ] = 1'b1;
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_PC;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_4;
+                basic_ctrl[bit_agu_src +: 2 ] = `ysyx_25110270_AGUSRC_RS1;
+                basic_ctrl[bit_op +: 3      ] = 3'b011;                      // for jalr, jal, remap funct3 to 3'b011 to simplify control logic
+            end
+            `ysyx_25110270_RV32I_OP_JAL: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = 1'b1;
+                basic_ctrl[bit_sign         ] = 1'b1;
+                basic_ctrl[bit_br_valid     ] = 1'b1;                        // br_valid + funct3 == 011 ---->  alu_add
+                basic_ctrl[bit_alu_srca +: 2] = `ysyx_25110270_ALUSRCA_PC;
+                basic_ctrl[bit_alu_srcb +: 2] = `ysyx_25110270_ALUSRCB_4;
+                basic_ctrl[bit_agu_src +: 2 ] = `ysyx_25110270_AGUSRC_PC;
+                basic_ctrl[bit_op +: 3      ] = 3'b011;                      // for jalr, jal, remap funct3 to 3'b011 to simplify control logic
+            end
+            `ysyx_25110270_RV_OP_CSR: begin
+                basic_ctrl[bit_rd_we        ] = 1'b1;
+                basic_ctrl[bit_rs1_re       ] = ~funct3[2];  // no csrrwi, csrrsi, csrrci
+                basic_ctrl[bit_csr_valid    ] = 1'b1;
+                basic_ctrl[bit_csr_src      ] = funct3[2];   // 1 for imm, 0 for rs1
+                basic_ctrl[bit_op +: 3      ] = funct3;
+            end
+            default: begin end
+        endcase
+    end
+
+    //------------------------------------------------------------------------
+    // 异常解码
+    //------------------------------------------------------------------------
+    wire [`ysyx_25110270_ExceptBus ] except;
+    ysyx_25110270_dec_except dec_except
+    (
+        .I_inst                 (I_inst                     ),
+        .O_except               (except                     )
+    );
+
+    assign O_except = except;
+
+    //------------------------------------------------------------------------
+    // csr重映射
+    //------------------------------------------------------------------------
+    wire [`ysyx_25110270_CsrMapBus ] csr_map;
+    ysyx_25110270_csr_mapout csr_mapout
+    (
+        .I_csr_addr             (I_inst[31:20]              ),
+        .O_csr_map              (csr_map                    )
+    );
+
+    assign O_csr_addr = csr_map;
+
+    //------------------------------------------------------------------------
+    // 输出
+    //------------------------------------------------------------------------
+    assign O_inst = I_inst;
+    assign O_inst_addr = I_inst_addr;
+    
+    assign O_ready = I_ready;
+    assign O_valid = O_ready;
+
+    assign O_rs1_raddr = rs1;
+    assign O_rs2_raddr = rs2;
+    assign O_rd_waddr = rd;
+
+    assign O_imm = imm;
+    assign O_rd_we = basic_ctrl[bit_rd_we];
+
+    assign O_op = basic_ctrl[bit_op +: 3];
+
+    assign O_ld_valid = basic_ctrl[bit_ld_valid];
+    assign O_st_valid = basic_ctrl[bit_st_valid];
+    assign O_csr_valid = basic_ctrl[bit_csr_valid];
+    assign O_br_valid = basic_ctrl[bit_br_valid];
+    assign O_f7b5_en = basic_ctrl[bit_f7b5_en];
+    assign O_sign = basic_ctrl[bit_sign];
+
+    assign O_rs1_re = basic_ctrl[bit_rs1_re];
+    assign O_rs2_re = basic_ctrl[bit_rs2_re];
+
+    assign O_alu_srca_sel = basic_ctrl[bit_alu_srca +: 2];
+    assign O_alu_srcb_sel = basic_ctrl[bit_alu_srcb +: 2];
+    assign O_agu_src_sel  = basic_ctrl[bit_agu_src +: 2];
+    assign O_csr_src_sel  = basic_ctrl[bit_csr_src];
+
+endmodule
+
+//------------------------------------------------------------------------
+// 指令解包模块
+//------------------------------------------------------------------------
+
+module ysyx_25110270_RV32_Inst_Unpack
+(
+    input   wire    [31:0]  I_inst,
+    output  wire    [6:0 ]  opcode,
+    output  wire    [2:0 ]  funct3,
+    output  wire    [6:0 ]  funct7,
+    output  wire    [3:0 ]  rd,
+    output  wire    [3:0 ]  rs1,
+    output  wire    [3:0 ]  rs2
+);
+
+    assign opcode = I_inst[6:0];
+    assign rd     = I_inst[10:7];
+    assign funct3 = I_inst[14:12];
+    assign rs1    = I_inst[18:15];
+    assign rs2    = I_inst[23:20];
+    assign funct7 = I_inst[31:25];
+
+endmodule
+
+//------------------------------------------------------------------------
+// CSR寄存器重映射
+//------------------------------------------------------------------------
+module ysyx_25110270_csr_mapout
+(
+    input   wire    [11:0                       ]   I_csr_addr,
+    output  wire    [`ysyx_25110270_CsrMapBus   ]   O_csr_map
+);
+
+    reg [`ysyx_25110270_CsrMapBus] csr_map;
+    always @(*) begin
+        case(I_csr_addr)
+            `ysyx_25110270_CSR_MSTATUS      : csr_map =  `ysyx_25110270_CSR_MAP_MSTATUS;
+            `ysyx_25110270_CSR_MIE          : csr_map =  `ysyx_25110270_CSR_MAP_MIE;
+            `ysyx_25110270_CSR_MTVEC        : csr_map =  `ysyx_25110270_CSR_MAP_MTVEC;
+            `ysyx_25110270_CSR_MEPC         : csr_map =  `ysyx_25110270_CSR_MAP_MEPC;
+            `ysyx_25110270_CSR_MCAUSE       : csr_map =  `ysyx_25110270_CSR_MAP_MCAUSE;
+            `ysyx_25110270_CSR_CYCLE        : csr_map =  `ysyx_25110270_CSR_MAP_CYCLE;
+            `ysyx_25110270_CSR_CYCLEH       : csr_map =  `ysyx_25110270_CSR_MAP_CYCLEH;
+            `ysyx_25110270_CSR_MVENDORID    : csr_map =  `ysyx_25110270_CSR_MAP_MVENDORID;
+            `ysyx_25110270_CSR_MARCHID      : csr_map =  `ysyx_25110270_CSR_MAP_MARCHID;
+            default                         : csr_map =  0;
+        endcase
+    end
+
+    assign O_csr_map = csr_map;
+
+endmodule
+
+//------------------------------------------------------------------------
+// 异常指令译码单元
+//------------------------------------------------------------------------
+module ysyx_25110270_dec_except
+(
+    input   wire    [31:0                       ]   I_inst,
+    output  wire    [`ysyx_25110270_ExceptBus   ]   O_except
+);
+
+    // 异常指令
+    assign O_except[`ysyx_25110270_EXCPT_ECALL  ] = (I_inst == `ysyx_25110270_RV_ECALL  );
+    assign O_except[`ysyx_25110270_EXCPT_EBREAK ] = (I_inst == `ysyx_25110270_RV_EBREAK );
+    assign O_except[`ysyx_25110270_EXCPT_MRET   ] = (I_inst == `ysyx_25110270_RV_MRET   );
+
+endmodule
