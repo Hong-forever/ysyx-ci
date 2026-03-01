3424138bdd594f736964ee97dd2c238faf920e1b
tracer-ysyx <tracer@ysyx.org>
2026-03-01 14:37:00 +0800

    > sim RTL for perf ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:37:00 up 5 days, 27 min,  1 user,  load average: 0.39, 0.76, 0.54

--- npc/vsrc/pipeline.v
+++ npc/vsrc/pipeline.v
@@ -105,6 +105,7 @@ module ysyx_25110270_pipeline_dec_ex
             O_br_valid      <= 0                        ;
             O_csr_valid     <= 0                        ;
             O_except        <= 0                        ;
+            O_inst_addr     <= 0                        ; // difftest 
         end else if(I_enable) begin
             O_inst          <= I_inst                   ;
             O_inst_addr     <= I_inst_addr              ;
@@ -178,6 +179,7 @@ module ysyx_25110270_pipeline_ex_wb
             O_rd_we         <= 0                        ;
             O_csr_valid     <= 0                        ;
             O_except        <= 0                        ;
+            O_inst_addr     <= 0                        ; // difftest 
         end else if(I_enable) begin
             O_inst          <= I_inst                   ;
             O_inst_addr     <= I_inst_addr              ;
