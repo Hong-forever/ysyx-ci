bd1dd4cd719cd831f46679a01283e035ac3d0cb9
tracer-ysyx <tracer@ysyx.org>
2026-03-03 14:38:20 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:38:20 up 7 days,  2:39,  1 user,  load average: 1.34, 0.57, 0.23

--- npc/vsrc/ysyx_25110270.v
+++ npc/vsrc/ysyx_25110270.v
@@ -2,8 +2,8 @@
 //------------------------------------------------------------------------
 // VERILOG MACRO
 //------------------------------------------------------------------------
-// `define ysyx_25110270_DPIC
-// `define ysyx_25110270_PERF
+`define ysyx_25110270_DPIC
+`define ysyx_25110270_PERF
 // `define ysyx_25110270_DEBUG
 // `define ysyx_25110270_SOC //no verilog macro define here, define it in Makefile
 
