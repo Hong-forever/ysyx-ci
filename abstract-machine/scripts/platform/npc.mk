3de295d8ba6396dad64a90ed278a9c1e4263132f
tracer-ysyx <tracer@ysyx.org>
2026-03-03 11:31:32 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:31:32 up 6 days, 23:27,  1 user,  load average: 0.44, 0.75, 0.50

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -13,7 +13,7 @@ AM_SRCS := riscv/npc/start.S \
 CFLAGS    += -fdata-sections -ffunction-sections
 CFLAGS    += -I$(AM_HOME)/am/src/riscv/npc/include
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
-LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --defsym=_pmem_start=0xa0000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 
 NPCFLAGS  += --log=$(shell dirname $(IMAGE).elf)/npc-log.txt
