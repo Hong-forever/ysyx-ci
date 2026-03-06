d563b686d5f949dfbfaae0846e9463bdc6c84b5f
hhh <3224318821@qq.com>
2026-03-06 14:42:59 +0800

    fix bug for strncmp

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -54,11 +54,12 @@ int strcmp(const char *s1, const char *s2) {
 int strncmp(const char *s1, const char *s2, size_t n) {
     size_t i;
     for(i=0; s1[i] != '\0' && s2[i] != '\0' && i<n; i++) {
-        if(s1[i] != s2[i])
+        if(s1[i] != s2[i]) {
             return s1[i] - s2[i];
+        }
     }
 
-    return s1[i] - s2[i];
+    return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
