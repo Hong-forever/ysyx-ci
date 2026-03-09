d6df8a6ed55744ba70969f32576f18aeb0054e5e
hhh <3224318821@qq.com>
2026-03-09 18:23:51 +0800

    uart and iverilog microbench

--- npc/vsrc/ysyx_25110270.v
+++ npc/vsrc/ysyx_25110270.v
@@ -2201,7 +2201,7 @@ module ysyx_25110270_wbu
             //     $display("~~~~~~~~~~#       #    #     #    ######~~~~~~~~~~");
             //     $display("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             //     $display("Finish at PC = 0x%08x", inst_addr_r1);
-            // end
+            end
             $finish;
         end
     end
