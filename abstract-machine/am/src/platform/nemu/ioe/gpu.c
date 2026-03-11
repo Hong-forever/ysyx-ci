39115cfc8d3dd9c0b8c6210bc26d918a17bc6fd0
tracer-ysyx <tracer@ysyx.org>
2026-03-11 22:08:47 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:08:47 up 2 days,  9:11,  1 user,  load average: 0.36, 0.45, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -40,7 +40,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
         for(int j=0; j<ctl->h; j++) {
             uint32_t *src = px + j * ctl->w;
             uint32_t *dst = fb + (ctl->y + j) * width + ctl->x;
-            memcpy(dst, src, ctl->w * sizeof(uint32_t));
+            for(int i=0; i<ctl->w; i++)
+                dst[i] = src[i];
         }
         
 
