7a4e1b12c68b8250a4088f72aaef74e0c4d5d9c8
tracer-ysyx <tracer@ysyx.org>
2026-03-12 19:39:07 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:39:07 up 3 days,  7:18,  1 user,  load average: 0.84, 0.39, 0.14

--- npc/scripts/difftest.mk
+++ npc/scripts/difftest.mk
@@ -2,7 +2,7 @@ ifdef CONFIG_DIFFTEST
 
 DIFF_REF_PATH = $(NEMU_HOME)
 
-remote_quote = $(patsubst "%",%,$(1))
+remove_quote = $(patsubst "%",%,$(1))
 
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-$(call remove_quote,$(CONFIG_ENGINE))-so
 
