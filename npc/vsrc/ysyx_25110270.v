41c4bc9b687b7629acb935a0489a6edc9dc24b7b
tracer-ysyx <tracer@ysyx.org>
2026-03-12 18:39:59 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:39:59 up 3 days,  6:17,  1 user,  load average: 0.30, 0.15, 0.10

--- npc/vsrc/ysyx_25110270.v
+++ npc/vsrc/ysyx_25110270.v
@@ -2875,7 +2875,8 @@ module ysyx_25110270_arbiter
 
 
     // AXI信号连接
-    assign M_awvalid  = state_m1 ? M1_awvalid : 0;
+    assign M_awvalid  = state_m0 ? M0_awvalid :
+                        state_m1 ? M1_awvalid : 0;
     assign M_wvalid   = M_awvalid;
     assign M_arvalid  = state_m0 ? M0_arvalid :
                         state_m1 ? M1_arvalid : 0;
