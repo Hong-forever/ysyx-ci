b288fb1ee67e846d164ffba66f1b14866ef0be20
tracer-ysyx <tracer@ysyx.org>
2026-03-23 20:46:16 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:46:16 up 10 days, 17:37,  1 user,  load average: 0.89, 0.63, 0.43

--- abstract-machine/am/src/riscv/ysyxsoc/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/uart.c
@@ -9,6 +9,7 @@ void __am_uart_tx(AM_UART_TX_T *uart) {
 void __am_uart_rx(AM_UART_RX_T *uart) {
     if(inb(SERIAL_PORT + 5) & 0x01) {
         uart->data = inb(SERIAL_PORT);
+        printf("uart: received '%c' (0x%02x)\n", uart->data, uart->data);
     } else {
         uart->data = (char)-1;
     }
