f46950733a0052ddf8b2e603cc40d8495c98999c
tracer-ysyx <tracer@ysyx.org>
2026-04-09 22:58:40 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:58:40 up 1 day,  9:55,  1 user,  load average: 0.23, 0.17, 0.11

--- abstract-machine/scripts/riscv32e-ysyxsoc.mk
+++ abstract-machine/scripts/riscv32e-ysyxsoc.mk
@@ -4,7 +4,7 @@ include $(AM_HOME)/scripts/platform/ysyxsoc.mk
 
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
 
-COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
+COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32   # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
 AM_SRCS += riscv/ysyxsoc/libgcc/div.S \
