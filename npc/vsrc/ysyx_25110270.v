cde96c07f19da9f73d3f1fc8db0faaf0e79ff3e8
hhh <3224318821@qq.com>
2026-04-10 21:43:29 +0800

    fix bug for csr data forward

--- npc/vsrc/ysyx_25110270.v
+++ npc/vsrc/ysyx_25110270.v
@@ -1220,7 +1220,7 @@ module ysyx_25110270_exec
     ysyx_25110270_csr_exe csr_exe
     (
         .I_csr_src                  (csr_src                ),
-        .I_csr_rdata                (I_csr_rdata            ),
+        .I_csr_rdata                (final_csr_rdata        ),
         .I_csr_ctrl                 (I_op[1:0]              ),
         .O_csr_wdata                (csr_wdata              )
     );
@@ -2304,7 +2304,7 @@ module ysyx_25110270_regfile
     assign O_gpr14 = regs[14];
     assign O_gpr15 = regs[15];
 
-    wire [31:0] gpr1_ra = regs[1];
+    wire [31:0] gpr_ra = regs[1];
     wire [31:0] gpr_sp = regs[2];
     wire [31:0] gpr_gp = regs[3];
     wire [31:0] gpr_tp = regs[4];
@@ -2324,7 +2324,7 @@ module ysyx_25110270_regfile
 
 `ifdef __ICARUS__
 
-    wire [31:0] gpr1_ra = regs[1];
+    wire [31:0] gpr_ra = regs[1];
     wire [31:0] gpr_sp = regs[2];
     wire [31:0] gpr_gp = regs[3];
     wire [31:0] gpr_tp = regs[4];
