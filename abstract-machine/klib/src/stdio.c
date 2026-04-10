ecefd8d519c06f365d9696eeaac9c15bee9d923b
tracer-ysyx <tracer@ysyx.org>
2026-04-10 10:55:36 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:55:36 up 1 day, 22:53,  1 user,  load average: 0.21, 0.56, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,7 +5,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define STRLEN 0x10000
+#define STRLEN 0x2000
 
 static char *print_int_to_buf(char *out, int num);
 static char *print_uint_to_buf(char *out, unsigned int num);
