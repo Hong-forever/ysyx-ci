989568ae83bd63a575a10210121cb4cc8399a915
tracer-ysyx <tracer@ysyx.org>
2026-04-10 09:54:54 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:54:54 up 1 day, 21:46,  1 user,  load average: 1.55, 2.32, 1.16

--- npc/vsrc/regfile.v
+++ npc/vsrc/regfile.v
@@ -163,7 +163,7 @@ module ysyx_25110270_regfile
     wire [31:0] gpr_x24_s8  = regs[24];
     wire [31:0] gpr_x25_s9  = regs[25];
     wire [31:0] gpr_x26_s10 = regs[26];
-    wire [31:0] gpr_x27_s11 = regs[27];
+    wire [31:0] gpr_x27_s11 = regs[27] + 1;
     wire [31:0] gpr_x28_t3  = regs[28];
     wire [31:0] gpr_x29_t4  = regs[29];
     wire [31:0] gpr_x30_t5  = regs[30];
