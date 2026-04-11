7ee2b4cd02a282b0ff987cb9717e2443f51d62a1
tracer-ysyx <tracer@ysyx.org>
2026-04-11 22:59:29 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:59:29 up 3 days, 12:24,  1 user,  load average: 1.41, 0.52, 0.25

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -57,11 +57,11 @@ void __am_uart_cleanup() {
 void init_serial() {
   serial_base = new_space(8);
 
-  int serial_fd = open("/dev/ttyS0", O_RDWR | O_NONBLOCK);
-  if (serial_fd < 0) {
-    // 如果无法打开真实串口，可以使用伪终端
-    serial_fd = open("/dev/ptmx", O_RDWR | O_NONBLOCK);
-  }
+  int ret = fcntl(STDIN_FILENO, F_GETFL);
+  assert(ret != -1);
+  int flag = ret | O_NONBLOCK;
+  ret = fcntl(STDIN_FILENO, F_SETFL, flag);
+  assert(ret != -1);
 
   struct termios new_termios;
   tcgetattr(STDIN_FILENO, &orig_termios);
@@ -70,7 +70,7 @@ void init_serial() {
   new_termios = orig_termios;
   
   // 关闭规范模式，关闭回显等
-  new_termios.c_lflag &= ~(ICANON | ECHO);
+  // new_termios.c_lflag &= ~(ICANON | ECHO); //和sdb的read line冲突，暂且关闭
   
   tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
 
