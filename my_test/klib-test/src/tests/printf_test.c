a0386b30bfc0772c8ca245d7b46b657c48da0dc9
tracer-ysyx <tracer@ysyx.org>
2026-04-11 22:19:06 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux DESKTOP-QVSAAQL 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:19:06 up 3 days, 11:42,  1 user,  load average: 0.02, 0.06, 0.08

--- my_test/klib-test/src/tests/printf_test.c
+++ my_test/klib-test/src/tests/printf_test.c
@@ -26,6 +26,7 @@ char char_test_cases[] = {
 };
 
 void printf_test() {
+    putstr("This function doesn't need newline.");
     for(int i=0; i<sizeof(int_test_cases)/sizeof(int_test_cases[0]); i++) {
         printf("%d ", int_test_cases[i]);
     }
